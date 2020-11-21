edb89e24a90982b693ab89c122d0645fc58bf7e7
NightmareManish <63774978+NightmareManish@users.noreply.github.com>
2020-11-21 21:22:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -107,33 +107,7 @@ client.on('message', message => {
             client.deadline.get('deadline-premleague').execute(message, args, Discord);
             break;
 
-        case 'kick':
-           
-            const user = message.mentions.users.first();
-
-            if (user) {
-
-                const member = message.guild.member(user);
-                if (member) {
-                    member.kick('You where Kicked from freak gamer').then(() => {
-                        message.reply(`Sucessfully kicked ${user.tag} https://tenor.com/view/kids-bye-kick-gif-13210062`);
-                    }).catch(err => {
-                        message.reply(' I was unable to kick thhe Member');
-                        console.log(err);
-
-
-                    });
-                } else {
-                    message.reply("That user isn\'t in this guild")
-                }
-            } else {
-
-                message.reply("You need to specify a Person!")
-            }
-            break;
-
-
-
+    
     }
 
 
