a5f1a92360fffeab4d1b95d123481b566509934a
NightmareManish <63774978+NightmareManish@users.noreply.github.com>
2020-11-21 22:18:07 +0530

    efootball pes

--- index.js
+++ index.js
@@ -109,28 +109,9 @@ client.on('message', message =>{
          
         case 'kick':
            
-            const user = message.mentions.users.first();
-
-            if (user) {
-
-                const member = message.guild.member(user);
-                if (member) {
-                    member.kick('You where Kicked from freak gamer').then(() => {
-                        message.reply(`Sucessfully kicked ${user.tag} https://tenor.com/view/kids-bye-kick-gif-13210062`);
-                    }).catch(err => {
-                        message.reply(' I was unable to kick the Member');
-                        console.log(err);
-
-
-                    });
-                } else {
-                    message.reply("That user isn\'t in this guild")
-                }
-            } else {
-
-                message.reply("You need to specify a Person!")
-            }
-            break;
+        client.commands.get('kick').execute(message,args, Discord);
+        break;
+            
 
 
         
