5a586ad76df3b60e06470e1cfc357599e1e9c004
NightmareManish <63774978+NightmareManish@users.noreply.github.com>
2020-11-22 12:57:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ client.on('message', message => {
             const member = message.guild.member(user);
             if (member) {
                 member.kick('You where Kicked from freak gamer').then(() => {
-                    message.reply(`Sucessfully kicked ${user.tag} https://tenor.com/view/kids-bye-kick-gif-13210062`);
+                    message.reply(`Sucessfully kicked ${user.tag} \nhttps://tenor.com/view/kids-bye-kick-gif-13210062`);
                 }).catch(err => {
                     message.reply(' I was unable to kick the Member');
                     console.log(err);
