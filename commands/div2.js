44c55eec3e5a20a2f2b817108fb77371e1f96b5f
NightmareManish <63774978+NightmareManish@users.noreply.github.com>
2020-11-23 12:31:52 +0530

    Update div2.js

--- commands/div2.js
+++ commands/div2.js
@@ -1,6 +1,6 @@
 module.exports ={
     name: 'div2',
-    Description: 'Gives Rank for Divion 2',
+    Description: 'Gives Rank for Divsion 2',
     execute(message,args, Discord){
      
        message.channel.send('https://ws.eniblo.com/ofYqu7h6m2OFn6RdKz/ranks.html');
