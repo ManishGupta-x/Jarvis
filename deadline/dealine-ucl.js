07eb984f8c7c4ca646f927c64dbe03b443a5478e
NightmareManish <63774978+NightmareManish@users.noreply.github.com>
2020-11-23 12:49:28 +0530

    Update dealine-ucl.js

--- deadline/dealine-ucl.js
+++ deadline/dealine-ucl.js
@@ -3,7 +3,7 @@ module.exports ={
     Description: 'shows deadline of UCL',
     execute(message,args, Discord){
      
-       message.channel.send('21st November, 2020 (Last Updated)');
+       message.channel.send('30th November, 2020 (Last Updated)');
 
 
     }  
