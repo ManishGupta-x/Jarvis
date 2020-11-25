7f05fee16823fe386e0e2d27cc226963ecae8378
Manish gupta <NightmareManish>
2020-11-25 18:50:35 +0530

    efootball pes

---
+++ commands/kick.js
@@ -0,0 +1,31 @@
+module.exports ={
+    name: 'kick',
+    Description: 'this is a kick command!',
+    execute(message,args, Discord){
+     
+        const user = message.mentions.users.first();
+        
+        
+        if (user) {
+
+            const member = message.guild.member(user);
+            if (member) {
+                member.kick('You where Kicked from freak gamer').then(() => {
+                    message.reply(`Sucessfully kicked ${user.tag} \nhttps://tenor.com/view/kids-bye-kick-gif-13210062`);
+                }).catch(err => {
+                    message.reply('I can\'t kick that member bruh');
+                    console.log(err);
+
+
+                });
+            } else {
+                message.reply("That user isn\'t in this guild")
+            }
+        } else {
+
+            message.reply("You need to specify a Person!")
+        }
+
+
+    }  
+}
--- index.js
+++ index.js
@@ -92,32 +92,11 @@ client.on('message', message => {
     }
     else if (command === 'kick') {
 
-       
-
-        const user = message.mentions.users.first();
-
         if(!message.member.roles.cache.has('599566802682511360') && !message.member.roles.cache.has('610377914109788180') )
         return message.channel.send('YOU DONT HAVE PERMISSION TO THAT DUMB ').then(message => message.delete({timeout: 4000}));
         
-        if (user) {
-
-            const member = message.guild.member(user);
-            if (member) {
-                member.kick('You where Kicked from freak gamer').then(() => {
-                    message.reply(`Sucessfully kicked ${user.tag} \nhttps://tenor.com/view/kids-bye-kick-gif-13210062`);
-                }).catch(err => {
-                    message.reply('I can\'t kick that member bruh');
-                    console.log(err);
-
-
-                });
-            } else {
-                message.reply("That user isn\'t in this guild")
-            }
-        } else {
-
-            message.reply("You need to specify a Person!")
-        }
+        client.commands.get('kick').execute(message, args, Discord);
+        
     }   else if (command === 'gn') {
             client.commands.get('gn').execute(message, args, Discord);
 
