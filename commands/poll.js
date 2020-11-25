4be4458e5f1414c0210b18f93269452d7d596236
Manish gupta <NightmareManish>
2020-11-25 20:08:34 +0530

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -8,10 +8,10 @@ module.exports ={
       if(!args[1]){
       
          message.channel.send('What are you expecting in poll ? such a dumb');
-
+         return;
        }
        
-       let msgArgs =args.slice(1).join(" ");
+       let msgArgs = args.slice(1).join(" ");
         message.channel.send(msgArgs).then(MessageReaction => {
         MessageReaction.react("🅰️");
         MessageReaction.react("🅱️");
