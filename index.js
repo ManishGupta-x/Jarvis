c5438096a0975b0c9f1a3be0ae41de657677724c
Manish gupta <NightmareManish>
2020-11-25 17:34:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ client.on('message', message => {
 
         const user = message.mentions.users.first();
 
-        if(!message.member.roles.cache.has(r => r.name === "Bot Manager") || !message.member.roles.cache.has(r => r.name === "🎖️│Freaky Staff") )
+        if(!message.member.roles.cache.has('599566802682511360') || !message.member.roles.cache.has('610377914109788180') )
         return message.channel.send('YOU DONT HAVE PERMISSION TO THAT ').then(message => message.delete({timeout: 4000}));
         
         if (user) {
