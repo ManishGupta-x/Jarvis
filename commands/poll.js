ae1fa325eb6d97d9e8f8b28bc788ca10f7cc6730
Manish gupta <NightmareManish>
2020-11-26 12:13:52 +0530

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -2,13 +2,13 @@ module.exports ={
     name: 'poll',
     Description: 'this is a poll command!',
     execute(message,args, Discord){
-        if(!args[1]){
+        if(!args[0]){
       
             message.channel.send('What are you expecting in poll ?');
             break;
           }
            
-          let msgArgs = args.slice(1).join(" ");
+          let msgArgs = args.slice(1).join("");
             message.channel.send(msgArgs).then(MessageReaction => {
             MessageReaction.react("🅰️");
             MessageReaction.react("🅱️");
