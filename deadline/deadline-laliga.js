186588e49deb44b0e5f137a6ac5eb6d8bd30f4b1
Manish gupta <NightmareManish>
2020-12-01 18:02:41 +0530

    Update deadline-laliga.js

--- deadline/deadline-laliga.js
+++ deadline/deadline-laliga.js
@@ -3,7 +3,7 @@ module.exports ={
     Description: 'shows deadline of la liga Ko tourney',
     execute(message,args, Discord){
      
-       message.channel.send('30th November ,2020 (Last Updated)');
+       message.channel.send('11th december ,2020 (Last Updated)');
 
 
     }  
