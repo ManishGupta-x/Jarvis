a91fa94650cee2645f7f4283c3f8d4f6a1b70a5b
Manish gupta <NightmareManish>
2020-12-01 17:03:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,11 @@ client.on('message', message => {
         .setcolor(0xffff00)
         .setTitle(" Initiate a poll")
         .setDescription("p!poll to iniate a match poll"); 
-        message.channel.send(Embed);
+
+        if(!args[1]){
+     message.channel.send(Embed);
+      break;   
+    }
        }
        
   
