4461b0c60bd3a7be7002e190a3bb5d99d2195569
Manish gupta <NightmareManish>
2020-12-03 15:07:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.once('ready', () => {
 });
 
 
-client.on('message', message => {
+client.on('message', async message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
