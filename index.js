1cbcc1330820cef234f21ac57a9baeb497723e8f
Manish gupta <NightmareManish>
2020-12-03 13:30:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ client.on('message', message => {
                 return message.channel.send('You did not mention / give the id of the channel you wanted to create the poll in!')
              }
              
-                   let question =  message.content.slice(bot.prefix.lenth+5+channel.id.length+3)
+                   let question =  message.content.slice(client.prefix.lenth+5+channel.id.length+3)
                    if(!question){
         
                        return message.channel.send('You didnt specified a question for the poll')
@@ -131,7 +131,7 @@ client.on('message', message => {
                     .setTitle(" New Poll")
                     .setDescription(question)
                     .setFooter(`${message.author.username} Created this Poll`) 
-                    let msg = awaitbot.channels.cache.get(channel.id).send(Embed)
+                    let msg = awaitclient.channels.cache.get(channel.id).send(Embed)
                     await msg.react('🅰️') 
                     await msg.react('🅱️')
         
