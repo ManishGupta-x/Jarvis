a07687db941d560ae5f83b554fcd11db6de5810b
Manish gupta <NightmareManish>
2020-12-04 18:04:04 +0530

    Update rewards-freakcup.js

--- Rewards/rewards-freakcup.js
+++ Rewards/rewards-freakcup.js
@@ -16,7 +16,7 @@ module.exports ={
            
            
            )
-           .setImage('https://cdn.discordapp.com/attachments/599555520172654613/780793008500506644/1637813.jpg');
+       .setImage('https://cdn.discordapp.com/attachments/599555520172654613/780793008500506644/1637813.jpg');
            
            
            message.channel.send(newEmbed);
