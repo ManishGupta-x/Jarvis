cd4b302489bcedfe1562f23a8d7ee514ccc1fb86
Manish gupta <NightmareManish>
2020-12-04 18:47:47 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -17,11 +17,12 @@ module.exports ={
             {name : "4. Deadline Commands " , value : `Gives Info for Deadline Commands`},
             {name : "5. Server Competitions" , value : `Gives Info For Our Server Competetions`}
              
-          .setFooter(`Type prefix (p!) and the number from above menu Eg: p!2 `)
-          .setimage('https://cdn.discordapp.com/attachments/610950416498425886/784406658075721758/WW-17-800x445.jpg')
+          
         
         
            )
+           .setFooter(`Type prefix (p!) and the number from above menu Eg: p!2 `)
+          .setimage('https://cdn.discordapp.com/attachments/610950416498425886/784406658075721758/WW-17-800x445.jpg')
          
            
            
