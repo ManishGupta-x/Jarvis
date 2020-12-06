13e5d9555b1079ea982b01856f92ed32c528fab2
Manish gupta <NightmareManish>
2020-12-06 23:33:38 +0530

    Update freakcup-qualifications.js

--- commands/freakcup-qualifications.js
+++ commands/freakcup-qualifications.js
@@ -6,21 +6,22 @@ module.exports ={
        const newEmbed = new MessageEmbed()
        .setColor('#FC4206')
        .setTitle('Qualifications Descriptions')
-       .setDescription(` 
+       .setDescription(`4 Ways To Enter `)
+       .addfield(
 
-       **1.  __Top 3 Players__ From Both Divisions Will Qualify For the Freak Cup **
-       
-       **2. __Semifinalists Of UEFA Champions League __ Will Only be Qualified For Freak Cup**
-       
-       **3. The Winner Of Main Squad KO ( Scheduled for 19th Nov) and Runner Up, will Qualify For Freak Cup**
-       
-       **4. Depending On the Common players among the above tourneys Maximum Top players form __La Liga and Premier League__ Will Be selected and will  Qualify For Freak Cup**
-       
-       __In Total there will be 16 players in the Cup__
-       
-       __**THESE RULES WILL BE APPLICABLE FROM THE CURRENT SEASON AND WILL GO AS IT IS FOR UPCOMING SEASONS**__:Take_L: 
-       
-       **BEST OF LUCK GUYS*`)
+              
+           {name: "1" , value: `__Top 3 Players__ From Both Divisions Will Qualify For the Freak Cup`},
+           {name: "2" , value: `__Semifinalists Of UEFA Champions League __ Will Only be Qualified For Freak Cup`},  
+           {name: "3" , value: ' Depending On the Common players among the above tourneys Maximum Top players form __La Liga and Premier League__ Will Be selected and will  Qualify For Freak Cup'},
+           {name: "4" , value: 'In Total there will be 16 players in the Cup '},
+        
+
+
+
+
+
+
+       )
        
            .setImage('https://cdn.discordapp.com/attachments/767787771816443966/776755041872642048/FIF_world_cup_trophy_on_grassjpg.jpg');
            
