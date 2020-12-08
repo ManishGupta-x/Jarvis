65bacdc5eb82dbca247371f641f4af6b38860754
Manish gupta <NightmareManish>
2020-12-08 21:31:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-
+const moment = require('moment');
 const client = new Discord.Client();
 const { MessageEmbed } = require('discord.js')
 
@@ -219,6 +219,37 @@ client.on('message', async message => {
 
             message.delete({ timeout: 2000 })
 
+
+        case 'memberinfo': 
+
+
+
+        if (member.presence.status === 'dnd') member.presence.status = 'Do Not Disturb';
+        if (member.presence.status === 'online') member.presence.status = 'Online';
+        if (member.presence.status === 'idle') member.presence.status = 'Idle';
+        if (member.presence.status === 'offline') member.presence.status = 'offline';
+    
+        let x = Date.now() - member.createdAt;
+        let y = Date.now() - message.guild.members.cache.get(member.id).joinedAt;
+        const joined = Math.floor(y / 86400000);
+    
+        const joineddate = moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss");
+        let status = member.presence.status;
+    
+        const userEmbed = new Discord.MessageEmbed()
+        .setAuthor(member.user.tag, member.user.displayAvatarURL())
+        .setTimestamp()
+        .setColor('BLUE')
+        .setImage(member.user.displayAvatarURL())
+        .addField("Member ID", member.id)
+        .addField('Roles', `<@&${member._roles.join('> <@&')}>`)
+        .addField("Account Created On:", ` ${moment.utc(member.user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
+        .addField('Joined the server At', `${joineddate} \n> ${joined} day(S) Ago`)
+        .addField("Status", status)
+    
+        message.channel.send(userEmbed);
+        break;
+    
 
 
     }
