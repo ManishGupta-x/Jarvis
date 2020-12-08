205ec67162fa7e65bd2ca6f2ce08c37cddaa6ad2
Manish gupta <NightmareManish>
2020-12-08 23:47:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -224,7 +224,7 @@ client.on('message', async message => {
         case 'info':
            
        let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]) || message.member;
-        if(!member) return message.channel.send('pls mention someone')
+        
 
 
         if (member.presence.status === 'dnd') member.presence.status = 'Do Not Disturb';
