62bb0ab3e6f7abe37039f068d85f223a53a37a56
Manish gupta <NightmareManish>
2020-12-08 23:31:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ client.on('message', async message => {
 
         case 'memberinfo':
            
-       let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
+       let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]) || message.member;
         if(!member) return message.channel.send('pls mention someone')
 
 
