798552ec3da1475f509753604c317635186e3c98
Manish gupta <NightmareManish>
2020-12-24 22:16:38 +0530

    Update friendly.js

--- commands/friendly.js
+++ commands/friendly.js
@@ -4,13 +4,20 @@ module.exports ={
     name: 'friendly',
     Description: 'this is a  Friendly  command!',
     async  execute(message,args, Discord,client){
-        const channel = '730714810614022228';
+        const channel = '650289693543628812';
         const hand = '🤝';
-        
         let member = message.member
-            let membertarget = message.guild.members.cache.get(member.id);
-           
-        let msg = await message.channel.send(`@here <@${membertarget.id}> is Challenging For friendly ! React Below to Accept his Challenge!`)
+        let membertarget = message.guild.members.cache.get(member.id);
+        const newEmbed = new Discord.MessageEmbed()
+       .setColor('00ff74')
+       .setTitle(`<@${membertarget.id}> is Challenging For friendly ! React Below to Accept his Challenge!`)
+       .setDescription('\n Type  `join-role @..  If you dont want to play friendlies or Hide channel')
+        
+        
+        message.channel.send('@here') 
+        let msg = await message.channel.send(newEmbed)
+        
+        
         await msg.react('🤝')
 
 
