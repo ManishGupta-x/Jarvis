4ec54935b9c137f4a604ffade5067bfc2d049215
Manish gupta <NightmareManish>
2020-12-24 22:07:50 +0530

    Update friendly.js

--- commands/friendly.js
+++ commands/friendly.js
@@ -23,7 +23,7 @@ module.exports ={
             if (reaction.message.channel.id == channel) {
                 if (reaction.emoji.name === hand) {
                     await reaction.message.guild.members.cache.get(user.id);
-                    message.reply(`<@${user.id} Accepeted Your Challenge`);
+                    message.reply(`<@${user.id}> Accepeted Your Challenge`);
                 }
                 } else {
                 return;
