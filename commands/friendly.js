535d64727c462d14d01a7ae0d2169003ee5fadab
Manish gupta <NightmareManish>
2020-12-24 18:37:13 +0530

    Update friendly.js

--- commands/friendly.js
+++ commands/friendly.js
@@ -9,7 +9,7 @@ module.exports ={
         if(usedCommand.has(message.author.id)){
             message.reply('https://tenor.com/view/slow-down-a-little-bit-anthony-mennella-culter35-slow-down-dont-rush-gif-17969625')
         } else {
-            let member = message.mentions.members.first() || message.member;
+            let member = message.mentions.members.first();
             let msg = await message.channel.send(`@here @${member.user.id} is Challenging For friendly ! React Below to Accept his Challenge!`)
             await msg.react('🤝')
             await msg.awaitReactions((reaction, user) => user.id == user.id && (reaction.emoji.name == '🤝'), { max: 1, time: 1800000 })
