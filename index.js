0359cc547a65a03aa19c16adf168bcb7ef6ec59c
Manish gupta <NightmareManish>
2020-12-24 20:01:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -65,11 +65,14 @@ client.on('message', async message => {
             message.reply('https://tenor.com/view/slow-down-a-little-bit-anthony-mennella-culter35-slow-down-dont-rush-gif-17969625')
         } else {
             let member = message.mentions.members.first();
+            
+
+
             let msg = await message.channel.send(`@here <@${member.user.id}> is Challenging For friendly ! React Below to Accept his Challenge!`)
             await msg.react('🤝')
             await msg.awaitReactions((reaction, user) => user.id == user.id && (reaction.emoji.name == '🤝'), { max: 1, time: 1800000 })
             .then(async collected => {
-                if (collected.first().emoji.name == '🤝') { return `<@${member.user.id}> <@${user.id}> Accepted Your Challenge !` }
+                if (collected.first().emoji.name == '🤝') { return `<@${member.user.id}> Someone Accepted Your Challenge !` }
                 else return message.channel.send(`Try after Sometime bruh <@${member.user.id}> ,No one accepted :(`);
 
 
