5b0da5415cb82da193f59ace371255e0429c9d1b
Manish gupta <NightmareManish>
2020-12-26 12:56:20 +0530

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -7,7 +7,7 @@ module.exports ={
        .setColor('#ff3600')
        .setTitle('My INFO')
        .setAuthor('Freak Gamer')
-       .setDescription(`Hi bruh ! My Name is Efootball Pes, pleased to meet You :)) \n\n The Botfather is Manish#3540 (Nightmare On server Freak Gamer! \n\n To get started with me you can type ``p!help`` `)
+       .setDescription("Hi bruh ! My Name is Efootball Pes, pleased to meet You :)) \n\n The Botfather is Manish#3540 (Nightmare On server Freak Gamer)! \n\n To get started with me you can type ``p!help`` ")
            .setImage('https://cdn.discordapp.com/attachments/730714810614022228/792290989058293790/skysports-lionel-messi-neymar_4470326.png');
            
            
