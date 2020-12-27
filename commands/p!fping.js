d805adbbfebd2ca5b7dd7129f4af7a1862c7ab04
Manish gupta <NightmareManish>
2020-12-27 21:00:50 +0530

    Update p!fping.js

--- commands/p!fping.js
+++ commands/p!fping.js
@@ -6,7 +6,7 @@ module.exports ={
        let member = message.member;
        let memberTarget = message.guild.members.cache.get(member.id);
        let Role = message.guild.roles.cache.find(role => role.name === '..'); 
-       if(message.member.roles.cache.has('791919963316682752')){
+       if(message.member.roles.cache.has('741525189602574387')){
         memberTarget.roles.remove(Role.id);
           }
        else{
