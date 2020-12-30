366239d88368a49ddd5fefa93df33d79d9234939
Manish gupta <NightmareManish>
2020-12-30 20:36:14 +0530

    e

--- index.js
+++ index.js
@@ -66,8 +66,7 @@ client.on('message', async message => {
 
         case 'ping': client.commands.get('ping').execute(message, args, Discord);
             break;
-        case 'ban': client.commands.get('ban').execute(message, args, Discord);
-            break;
+       
         case 'friendly':client.commands.get('friendly').execute(message, args, Discord,client);
            
 
