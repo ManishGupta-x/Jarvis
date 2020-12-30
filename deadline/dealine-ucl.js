59e447dec3ba4ff1fe83f6c19fbcda87d9115f50
Manish gupta <NightmareManish>
2020-12-30 19:20:16 +0530

    Update dealine-ucl.js

--- deadline/dealine-ucl.js
+++ deadline/dealine-ucl.js
@@ -3,7 +3,7 @@ module.exports ={
     Description: 'shows deadline of UCL',
     execute(message,args, Discord){
      
-       message.channel.send('13th December, 2020 (Semis)');
+       message.channel.send('17th January 2021 (Group stage)');
 
 
     }  
