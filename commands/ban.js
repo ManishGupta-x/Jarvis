4f8cd2575c9da66d80411dc4dd375e790b8ba3e6
Manish gupta <NightmareManish>
2020-12-31 15:41:13 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,33 +1,31 @@
 module.exports ={
-    name: 'ban',
-    Description: 'this is a  Ping command!',
+    name: 'kick',
+    Description: 'this is a kick command!',
     execute(message,args, Discord){
-        
-             
-                const user = message.mentions.users.first();
-                
-                
-                if (user) {
-        
-                    const member = message.guild.member(user);
-                    if (member) {
-                        member.ban('banned').then(() => {
-                            message.reply(`Sucessfully kicked ${user.tag} `);
-                        }).catch(err => {
-                            message.reply('I can\'t kick that member bruh');
-                            console.log(err);
+     
+        const user = message.mentions.users.first();
         
         
-                        });
-                    } else {
-                        message.reply("That user isn\'t in this guild")
-                    }
-                } else {
-        
-                    message.reply("You need to specify a Person!")
-                }
-        
-        
-            }  
+        if (user) {
+
+            const member = message.guild.member(user);
+            if (member) {
+                member.ban('You were banned').then(() => {
+                    message.reply(`Sucessfully banned ${user.tag} `);
+                }).catch(err => {
+                    message.reply('I can\'t kick that member bruh');
+                    console.log(err);
+
+
+                });
+            } else {
+                message.reply("That user isn\'t in this guild")
+            }
+        } else {
+
+            message.reply("You need to specify a Person!")
         }
-    
+
+
+    }  
+}
