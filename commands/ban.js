f8f03ad6261065fe8af6623ed1f11b885437fbce
Manish gupta <NightmareManish>
2020-12-31 15:19:55 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -11,17 +11,17 @@ module.exports ={
 
     //args system that is very required!!!!
     let messageArray = message.content.split(" ")
-    let args = messageArray.slice(1);
+    let args1 = messageArray.slice(1);
 
  
 
     {
-        let toBan = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]);
+        let toBan = message.mentions.members.first() || message.guild.members.cache.get(args1[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args1.slice(0).join(" ") || x.user.username === args1[0]);
 
         if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You need permissions!") 
         if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("Bot need permissions!") 
 
-        const reason = args[1] || "There was no reason!";
+        const reason = args1[1] || "There was no reason!";
 
         toBan.ban({
             reason: reason
