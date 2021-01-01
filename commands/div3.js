a7e256534d5070395d0a1627ad9d36c3985743ff
Manish gupta <NightmareManish>
2021-01-01 12:15:41 +0530

    Update div3.js

--- commands/div3.js
+++ commands/div3.js
@@ -1,6 +1,6 @@
 module.exports ={
     name: 'div3',
-    Description: 'Gives Rank for Divion 1',
+    Description: 'Gives Rank for Divion 3',
     execute(message,args, Discord){
      
        message.channel.send('https://ws.eniblo.com/qsbUjSWfngZBiK4ZfC/');
