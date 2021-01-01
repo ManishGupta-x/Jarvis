d7d84d2d05e000f4fffc379bf1c5b02dff704798
Manish gupta <NightmareManish>
2021-01-01 12:15:11 +0530

    e

--- commands/div3.js
+++ commands/div3.js
@@ -1,9 +1,9 @@
 module.exports ={
-    name: 'div1',
+    name: 'div3',
     Description: 'Gives Rank for Divion 1',
     execute(message,args, Discord){
      
-       message.channel.send('https://ws.eniblo.com/j43U6FMw3zLkpLf8iq/ranks.html');
+       message.channel.send('https://ws.eniblo.com/qsbUjSWfngZBiK4ZfC/');
 
 
     }  
