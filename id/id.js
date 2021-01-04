c46f66a46699cdc3f9b0382a86fafc336c3dba75
Manish gupta <NightmareManish>
2021-01-04 20:05:30 +0530

    e

--- id/id.js
+++ id/id.js
@@ -1,57 +1,83 @@
 const fs = require("fs");
-const mongoose =require('mongoose');
- mongoose.connect('mongodb+srv://Manish:m7827851250@pesmobile.zolll.mongodb.net/test', { useNewUrlParser: true , useUnifiedTopology: true});
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://Manish:m7827851250@pesmobile.zolll.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
- const Data = require('../id/data.js');
+const Data = require('../id/data.js');
 
 
- module.exports ={
+module.exports = {
     name: 'id',
     Description: 'this is a  Ping command!',
-    execute(client,message,args, Discord){
-     
-       if(!args[0]){
+    async execute(client, message, args, Discord) {
 
-          user = message.author;
+        if (!args[0]) {
 
-       }else { user = message.mentions.users.first() || client.users.cache.get(args[0])   ;
+            user = message.author;
+
+        } else {
+            user = message.mentions.users.first() || client.users.cache.get(args[0]);
         }
 
         Data.findOne({
 
-           
-        }, (err,data) => {
-            if(err) console.log(err);
-            if(!data){
-                message.channel.send("You Have no ID set Yet You want to set? (Yes/no)")
-                var msg;
-                let msg1= message.channel.cache.get(msg);
-                
-                if(msg1 == yes || msg1 == Yes || msg1 == YES ){
-                    message.channel.send('Type your id Plss');
 
-                    let msg2= message.channel.cache.get(msg);
-                    const newdata = new Data({
-                        name: client.users.cache.get(user.id).username,
-                        userID : user.id,
-                        ID : msg2,
-                    })
-                    newdata.save().catch(err => console.log(err));
-                   data.save().catch(err => console.log(err));
-                   message.reply(' Id Updated ')
+        }, (err, data) => {
+            if (err) console.log(err);
+            if (!data) {
 
-                }else {
-                    message.send(' Id update Cancelled')
-                }              
-                
-              
-            
+
+                message.channel.send('You want to set your id? (yes/no)').then(() => {
+                    message.channel.awaitMessages({ max: 1, time: 30000, errors: ['time'] })
+                        .then(collected => {
+
+                            if ('yes' == collected.first() || 'Yes' == collected.first()) {
+                                message.channel.send('type your id pls').then(() => {
+                                    message.channel.awaitMessages({ max: 1, time: 30000, errors: ['time'] })
+                                        .then(collected => {
+
+                                            konamiid = collected.first();
+                                        })
+                                    message.send.channel('Confirm').then(() => {
+                                        message.channel.awaitMessages({ max: 1, time: 30000, errors: ['time'] }).then(() => {
+                                            if ('confirm' == collected.first() || 'Confirm' == collected.first()) {
+                                                const newdata = new Data({
+                                                    name: message.author.username,
+                                                    userID: message.author.id,
+                                                    ID: konamiid,
+                                                })
+                                                newdata.save().catch(err => console.log(err));
+                                                message.channel.send(`${data.ID} `);
+                                                data.ID = konamiid;
+                                                data.save().catch(err => console.log(err));
+                                                 message.reply('Your Id Has been set !')
+                                            }
+
+
+                                        })
+
+                                    })
+
+
+                                })
+                            } else { message.reply('Cancelled'); }
+                        })
+
+
+                })
+
+
+
             } else {
                 return message.channel.send(`${client.users.cache.get(user.id).username}'s Id ${data.ID}`);
             }
         }
         )
-     
-    }  
+
+    }
 }
+
+
+
+
+
