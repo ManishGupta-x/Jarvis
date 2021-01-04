5bd8743f19a06ee13dc46a626959fa59c36e6568
Manish gupta <NightmareManish>
2021-01-05 00:15:30 +0530

    Update id.js

--- id/id.js
+++ id/id.js
@@ -30,19 +30,14 @@ module.exports = {
             if(!args[0]){ 
             if (!data) {
                
-                const newdata = new Data({
-                    name: message.author.username,
-                    userID: message.author.id,
-                    Konami : args[0],
-                })
-                newdata.save().catch(err => console.log(err));
+                
                 return message.reply('you dont have any id set ')
 
              } else {
                 return message.channel.send(`${client.users.cache.get(user.id).username}'s Id ${data.Konami}`);
             }
         }else{
-            args[0] = data.Konami;
+              data.Konami = args[0];
             const newdata = new Data({
                 name: message.author.username,
                 userID: message.author.id,
