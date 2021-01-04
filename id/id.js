64dab06ab9254e56bad49f73544cf156bd0db017
Manish gupta <NightmareManish>
2021-01-04 20:49:24 +0530

    Update id.js

--- id/id.js
+++ id/id.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://Manish:m7827851250@pesmobile.zolll.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true });
-
+const filter = response => {
+	return (message => message.toLowerCase() === response.content.toLowerCase());
+};
 
 const Data = require('../id/data.js');
 
@@ -28,18 +30,18 @@ module.exports = {
 
 
                 message.channel.send('You want to set your id? (yes/no)').then(() => {
-                    message.channel.awaitMessages({ max: 1, time: 30000, errors: ['time'] })
+                    message.channel.awaitMessages(filter ,{ max: 1, time: 30000, errors: ['time'] })
                         .then(collected => {
 
                             if ('yes' == collected.first() || 'Yes' == collected.first()) {
                                 message.channel.send('type your id pls').then(() => {
-                                    message.channel.awaitMessages({ max: 1, time: 30000, errors: ['time'] })
+                                    message.channel.awaitMessages(filter ,{ max: 1, time: 30000, errors: ['time'] })
                                         .then(collected => {
 
                                             konamiid = collected.first();
                                         })
                                     message.send.channel('Confirm').then(() => {
-                                        message.channel.awaitMessages({ max: 1, time: 30000, errors: ['time'] }).then(() => {
+                                        message.channel.awaitMessages(filter ,{ max: 1, time: 30000, errors: ['time'] }).then(() => {
                                             if ('confirm' == collected.first() || 'Confirm' == collected.first()) {
                                                 const newdata = new Data({
                                                     name: message.author.username,
