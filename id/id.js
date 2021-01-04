498485cdcf1b8b65b2eaec594f858a8c2b4fb45e
Manish gupta <NightmareManish>
2021-01-04 15:48:05 +0530

    Update id.js

--- id/id.js
+++ id/id.js
@@ -30,7 +30,7 @@ const mongoose =require('mongoose');
                     id : "no id"
                 })
               newdata.save().catch(err => console.log(err));
-              return message.channel.send(`${client.users.cache.get(user.id).username} NO Id Yet`);
+              return message.channel.send(`NO Id Yet For ${client.users.cache.get(user.id).username} `);
             } else {
                 return message.channel.send(`${client.users.cache.get(user.id).username}'s Id ${data.id}`);
             }
