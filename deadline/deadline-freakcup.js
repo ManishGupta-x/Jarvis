912ad6650ed01a88b5b3f11eb54502c9345b3f55
Manish gupta <NightmareManish>
2021-01-05 23:15:46 +0530

    Update deadline-freakcup.js

--- deadline/deadline-freakcup.js
+++ deadline/deadline-freakcup.js
@@ -3,7 +3,7 @@ module.exports ={
     Description: 'shows deadline of Freak cup tourney',
     execute(message,args, Discord){
      
-       message.channel.send('5th December, 2020 (final)');
+       message.channel.send('10th Jan, 2021 (R16)');
 
 
     }  
