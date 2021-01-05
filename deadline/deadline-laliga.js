99b22229df709ecea44191af2f6338a46c28b018
Manish gupta <NightmareManish>
2021-01-05 13:04:46 +0530

    e

--- deadline/deadline-laliga.js
+++ deadline/deadline-laliga.js
@@ -3,7 +3,7 @@ module.exports ={
     Description: 'shows deadline of la liga Ko tourney',
     execute(message,args, Discord){
      
-       message.channel.send('20th december ,2020 (Last Updated)');
+       message.channel.send('11th January ,2020 (Last Updated)');
 
 
     }  
--- deadline/deadline-premleague.js
+++ deadline/deadline-premleague.js
@@ -3,7 +3,7 @@ module.exports ={
     Description: 'shows deadline of Premier league tourney',
     execute(message,args, Discord){
      
-       message.channel.send('2nd January ,2021 (Last Updated)');
+       message.channel.send('11th January ,2021 (Last Updated)');
 
 
     }  
