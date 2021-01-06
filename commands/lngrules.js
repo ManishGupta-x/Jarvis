0e2976be45a781a009c092d409955337d4635c9d
Manish gupta <NightmareManish>
2021-01-06 12:32:17 +0530

    Update lngrules.js

--- commands/lngrules.js
+++ commands/lngrules.js
@@ -9,7 +9,7 @@ module.exports ={
        .setAuthor('Freak Gamer')
        .addFields(
         
-           {name: "Rule 1" , value: ' if you are having General talks you can Have that any language You feel Like with your friends!'},
+           {name: "Rule 1" , value: ' if you are having General talks you can Have that in  any language You feel Like with your friends!'},
            {name: "Rule 2" , value: ' If you are asking queries or Talking about PES You need to talk in respective Channel Only or you will be 1st verbally warned and then a punishment will be filed! '},
            {name: "Rule 3" , value: ' 3 Warns = 2 Days  Mute! and after that a Kick and the a ban !  '}  
            
