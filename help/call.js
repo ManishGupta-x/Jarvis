bc665e8c0a952b5e1f77bc019d40e025dbe38ddd
Manish gupta <NightmareManish>
2021-01-07 19:14:57 +0530

    e

---
+++ help/call.js
@@ -0,0 +1,71 @@
+client.help = new Discord.Collection();
+const helpFiles = fs.readdirSync('./help/').filter(file => file.endsWith('.js'));
+for (const file of helpFiles) {
+
+    const command = require(`./help/${file}`);
+    client.help.set(command.name, command);
+
+}
+module.exports ={
+    name: 'call',
+    Description: 'this is a  Ping command!',
+    execute(client,message,args, Discord,msg){
+        const newEmbed2 = new Discord.MessageEmbed()
+        .setColor('#6beea8')
+        .setTitle('Help Module')
+        .setDescription('Server\'s Info And Commands')
+        .addFields(
+    
+            {
+                name: "1. General Commands ", value: `Gives Info for General commands like ping 
+         , twitter , friendly,Other Websites...etc`},
+            { name: "2. Rewards", value: `Gives Info for Rewards Commands` },
+    
+            { name: "3. Tourneys Info And Rules", value: `Gives Info and rules for Tourneys ` },
+            { name: "4. Deadline Commands ", value: `Gives Info for Deadline Commands` },
+            { name: "5. Server Competitions", value: `Gives Info For Our Server Competetions` },
+            { name: "6. Server Roles", value: `Gives Info for For diffrents roles on the server :)` }
+    
+    
+    
+    
+    
+        )
+        .setFooter(`You can only React for one at a time`)
+        .setImage('https://cdn.discordapp.com/attachments/610950416498425886/784406658075721758/WW-17-800x445.jpg');
+        let msg3 = await msg.edit(newEmbed2)
+        await msg3.react('1️⃣')
+        await msg3.react('2️⃣')
+        await msg3.react('3️⃣')
+        await msg3.react('4️⃣')
+        await msg3.react('5️⃣')
+        await msg3.react('6️⃣')
+        
+        
+        await msg3.awaitReactions((reaction, user) => user.id == user.id && user.id !== "778267007439077396" && (reaction.emoji.name == '1️⃣' ||
+        
+            reaction.emoji.name == '2️⃣' || reaction.emoji.name == '3️⃣' || reaction.emoji.name == '4️⃣' || reaction.emoji.name == '5️⃣' || reaction.emoji.name == '6️⃣' || reaction.emoji.name == '↩️'), { max: 1, time: 40000 })
+            .then(async collected => {
+                if (collected.first().emoji.name == '1️⃣') { return client.help.get('1').execute(message, args, Discord,msg) }
+                else if (collected.first().emoji.name == '2️⃣') { return client.help.get('2').execute(message, args, Discord) }
+                else if (collected.first().emoji.name == '3️⃣') { return client.help.get('3').execute(message, args, Discord) }
+                else if (collected.first().emoji.name == '4️⃣') { return client.help.get('4').execute(message, args, Discord) }
+                else if (collected.first().emoji.name == '5️⃣') { return client.help.get('5').execute(message, args, Discord) }
+                else if (collected.first().emoji.name == '6️⃣') { return client.help.get('6').execute(message, args, Discord) }
+                
+                else return message.channel.send('Time Up');
+        
+        
+        
+        
+        
+            }).catch(async () => { return message.channel.send("error") });
+        
+        }
+        
+        }
+        
+        
+
+
+   
