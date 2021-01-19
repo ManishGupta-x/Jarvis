54a36375676d8326d3559fd69cec395e3292cc0b
Manish gupta <NightmareManish>
2021-01-19 12:17:21 +0530

    Create pesmeme.js

---
+++ commands/pesmeme.js
@@ -0,0 +1,40 @@
+const got = require('got');
+const subReddits = ["pesmobile"]
+const rd = subReddits[Math.floor(Math.random() * subReddits.length)];
+
+
+module.exports ={
+    name: 'pesmeme',
+    Description: 'this is a  meme command!',
+    async execute(message,args, Discord){
+     
+        const embed = new Discord.MessageEmbed()
+        got(`https://www.reddit.com/r/${rd}/random/.json`).then(response => {
+            let content = JSON.parse(response.body);
+            let permalink = content[0].data.children[0].data.permalink;
+            let flair = content[0].data.children[0].data.flair;
+            let memeUrl = `https://reddit.com${permalink}`;
+            let memeImage = content[0].data.children[0].data.url;
+            let memeTitle = content[0].data.children[0].data.title;
+            let memeUpvotes = content[0].data.children[0].data.ups;
+            let memeDownvotes = content[0].data.children[0].data.downs;
+            let memeNumComments = content[0].data.children[0].data.num_comments;
+            embed.setTitle(`${memeTitle}`)
+            embed.setURL(`${memeUrl}`)
+            embed.setImage(memeImage)
+            embed.setColor('RANDOM')
+            embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`)
+            if (flair == 'meme'){
+                message.channel.send(embed);
+            }
+            else{
+                message.channel.send('NO meme Found');
+                
+            }
+            
+        
+
+    } )
+    }
+}
+
