8da13c063510ad7cd86b07f726591b44da8f5e48
Manish gupta <NightmareManish>
2021-02-11 12:27:49 +0530

    Create deadline-worldcup.js

---
+++ deadline/deadline-worldcup.js
@@ -0,0 +1,10 @@
+module.exports ={
+    name: 'deadline-worldcup',
+    Description: 'shows deadline of world cup tourney',
+    execute(message,args, Discord){
+     
+       message.channel.send('4th March, 2021 (Group Stage)');
+
+
+    }  
+}
