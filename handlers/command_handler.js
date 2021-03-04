68f022e9d468c9ee3ee02e71e7b4781aa75d398f
Manish gupta <NightmareManish>
2021-03-04 23:21:22 +0530

    Update command_handler.js

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = (client , Discord) =>{
   const command_files =  fs.readdirSync('./commands/').filter( file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
+for (const file of command_files) {
 
     const command = require(`./commands/${file}`);
     if(command.name){
