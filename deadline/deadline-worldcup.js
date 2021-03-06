25f55031faea7f41813904b6d87548177ee89754
Manish gupta <NightmareManish>
2021-03-06 11:12:36 +0530

    Update deadline-worldcup.js

--- deadline/deadline-worldcup.js
+++ deadline/deadline-worldcup.js
@@ -3,7 +3,7 @@ module.exports ={
     Description: 'shows deadline of world cup tourney',
     execute(message,args, Discord){
      
-       message.channel.send('4th March, 2021 (Group Stage)');
+       message.channel.send('14th March, 2021 (Quaterfinals)');
 
 
     }  
