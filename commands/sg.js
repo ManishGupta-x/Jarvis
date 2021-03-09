79972a86c888dd8dfc29c99def681e2fbf08f2c8
Manish gupta <NightmareManish>
2021-03-09 10:07:59 +0530

    Update sg.js

--- commands/sg.js
+++ commands/sg.js
@@ -1,7 +1,7 @@
 module.exports ={
     name: 'sg',
     Description: 'Gives sg Info !',
-    execute(client,message,args,Discord){
+    execute(message,args,Discord){
      
        const newEmbed = new Discord.MessageEmbed()
        .setColor('RANDOM')
