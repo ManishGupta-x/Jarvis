d5f1b06dda043569428ef7d50e6ac345a76e1c26
Manish gupta <NightmareManish>
2021-04-07 17:51:30 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,12 +3,7 @@ module.exports ={
      Description: 'this is a  Ping command!',
      execute(message,args, Discord){
       
-      message.channel.send({embed: { 
-         color: 16777215, description:'Test', 
-         image:  {
-             url: "https://i.imgur.com/ONngPJY.gifv"
-         }
-     }});
+        message.channel.send('pong!');
 
 
      }  
