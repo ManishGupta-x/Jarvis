8c5f1e7607cec23150167ad2f871cf8699e911f5
Manish gupta <NightmareManish>
2021-04-18 22:21:37 +0530

    e

---
+++ commands/welcome.js
@@ -0,0 +1,25 @@
+module.exports ={
+    name: 'welcome',
+    Description: 'Give id info !',
+    execute(message,args,Discord){
+     
+       const newEmbed = new Discord.MessageEmbed()
+       .setColor('RANDOM')
+       .setTitle('WELCOME TO FREAK GAMER')
+       .setDescription('Have a Pleasant Stay in the Server')
+       .setAuthor('Freak Gamer')
+       .addFields(
+        
+        {name: "Type !freakgamer " , value: 'Get whole server info'},
+        {name: "Read #announcemtns" , value: 'To see Current Season Tourneys Schedule'},
+        
+        
+        
+        )
+          
+           
+           
+           message.channel.send(newEmbed);
+
+       }
+    }
