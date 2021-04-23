0c71868a9d2c297201fa43da5e8683e4e27d600a
Manish gupta <NightmareManish>
2021-04-24 01:39:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const moment = require('moment');
-const prefix = 'j!';
+const prefix = 'j!'; 
 const mongoose = require('mongoose');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 const fetch = require("node-fetch").default;
