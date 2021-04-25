e820d69a0e2c1544a310ab04a3ddc4b122acaeb6
Manish gupta <NightmareManish>
2021-04-25 13:27:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -305,6 +305,8 @@ client.on('message', async message => {
                 break;
             case 'gn': client.commands.get('gn').execute(message, args, Discord);
                 break;
+             case 'htmlcodes': message.channel.send('https://htmlcolorcodes.com/')
+                break;
 
             case 'poll': if (!message.member.roles.cache.has('599566802682511360') && !message.member.roles.cache.has('610377914109788180'))
                 return message.channel.send('YOU DONT HAVE PERMISSION TO THAT DUMB ').then(message => message.delete({ timeout: 4000 })).catch(console.error);
