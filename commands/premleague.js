fe338db3199b3d1c08da6b32b4d3363c587ee6af
Manish gupta <NightmareManish>
2021-05-01 10:42:55 +0530

    Update premleague.js

--- commands/premleague.js
+++ commands/premleague.js
@@ -1,7 +1,7 @@
 module.exports ={
     name: 'premleague',
     Description: 'Gives premleague  Info !',
-    execute(client,message,args,Discord){
+    execute(message,args,Discord){
      
        const newEmbed = new Discord.MessageEmbed()
        .setColor('#9400ff')
@@ -10,7 +10,7 @@ module.exports ={
        .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/784406658075721758/WW-17-800x445.jpg')
        .addFields(
         
-           {name: "Rule 1" , value: ' Only  10 Premier league  players allowed In the whole Squad ( +LEGENDS and IMs) i.e 10 players in the WHOLE SQUAD including SUBS'},
+           {name: "Rule 1" , value: ' All Premier League Players in the whole Squad ( +LEGENDS and IMs) i.e 18 players in the WHOLE SQUAD including SUBS ( You can use White Balls but you can\'t sub them!'},
            {name: "Rule 2" , value: ' NO legends Allowed Other than Premier League  '},
            {name: "Rule 3" , value: ' If You will break the squad requirements the opponent will get the win !! And that will be final , So save your\'s in squads ! '},
            {name: "Rule 4" , value: ' Its a  One Legged Tourney !'}
