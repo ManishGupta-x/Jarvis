fd443477bacf528d0d363f448240c9eda5a5b266
Manish gupta <NightmareManish>
2021-05-02 15:39:52 +0530

    Update twitter.js

--- commands/twitter.js
+++ commands/twitter.js
@@ -1,7 +1,7 @@
 module.exports ={
     name: 'twitter',
     Description: 'Gives twitter link of Efootball Pes',
-    execute(client,message,args, Discord){
+    execute(message,args, Discord){
      
        message.channel.send('https://twitter.com/officialpes');
 
