9cada8096c15b2548250e995ecc5ad07035c4e4e
Manish gupta <NightmareManish>
2021-05-08 09:56:55 +0530

    Update janeman.js

--- commands/janeman.js
+++ commands/janeman.js
@@ -5,7 +5,7 @@ module.exports ={
     Description: 'this is a role command!',
     execute(message,args, Discord){
      
-        const channel = client.channels.cache.find(channel => channel.id === '745878533246681170')
+        
         if (!args[0]) {
 
            return message.channel.send('You didnt specified a sentence')
@@ -15,7 +15,7 @@ module.exports ={
            
        
            
-            channel.send(args[0]);
+       client.channels.cache.get(`745878533246681170`).send(args[0])
            message.delete({ timeout: 2000 })
 
 
