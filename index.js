53b3407628c597dab2cbac2a71a3b1aa4f0291ee
Manish gupta <NightmareManish>
2021-05-08 09:08:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -312,8 +312,7 @@ client.on('message', async message => {
                 break;
             case 'htmlcodes': message.channel.send('https://htmlcolorcodes.com/')
                 break;
-             case 'jahn':if (!message.member.roles.cache.has('599566802682511360') && !message.member.roles.cache.has('610377914109788180'))
-                return message.channel.send('YOU DONT HAVE PERMISSION TO THAT DUMB ').then(message => message.delete({ timeout: 4000 })).catch(console.error);
+             case 'jahn':
                     
                 const channel = '745878533246681170';
     
