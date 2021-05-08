67639820807fc6729558c8523f83f8b530845b0f
Manish gupta <NightmareManish>
2021-05-08 09:38:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -314,7 +314,7 @@ client.on('message', async message => {
                 break;
              case 'janeman': 
                     
-               const channel = '745878533246681170';
+                channel = '745878533246681170';
     
                  if (!args[0]) {
     
