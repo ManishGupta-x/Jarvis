b3cee99fe8eb8eff5353fee54e0588d2bb29e70e
Manish gupta <NightmareManish>
2021-05-15 15:52:07 +0530

    Update ucl.js

--- commands/ucl.js
+++ commands/ucl.js
@@ -10,7 +10,7 @@ module.exports ={
        .setDescription(' RULES')
        .addFields(
         
-           {name: "Rule 1" , value: '  9 Players Of the __SAME CLUB__ Must be On the ground for 90 mins. [ Not the 9 same players ,you can sub a club player with a club player and non-club with a club or non-club one]'},
+           {name: "Rule 1" , value: '  11 Players Of the __SAME CLUB__ Must be On the ground for 90 mins. [ Not the 11 same players ,you can sub a club player with a club player and non-club with a club or non-club one]'},
            {name: "Rule 2" , value: '  Legends and IM\'s Who played for __That CLUB __ are only ALLOWED**[ NO other legends are allowed]'},  
            {name: "Rule 3" , value: '  4 Slots for 1 club'},
            {name: "Rule 4" , value: ' Group Stage: Players Have to play only __1 Leg__ With Every Player'},
