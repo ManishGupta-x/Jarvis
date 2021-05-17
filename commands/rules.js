e7f895741e66b6d6c62e00cb53da08384558c3a0
Manish gupta <NightmareManish>
2021-05-17 22:35:43 +0530

    Update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -10,7 +10,7 @@ module.exports ={
        .setAuthor('Freak Gamer', 'https://cdn.discordapp.com/avatars/549989000115519503/e745abd8a0a5b7fd392d51a5a431755e.png?size=256')
        .addFields(
         
-           {name: "Rule 1" , value: ' Long balls from own half and Backpassing are banned in the Tourneys , If Your Opponent Performs these Long balls and you concede a GOAL , You will Report recorded proof in #proves channel and THE FINAL DECISION WILL BE TAKEN BY THE STAFF MEMBERS And THAT SHOULD BE Respected . ONLY IF YOU Concede Goal by that , you can do Long ball but remember you must not score with that'},
+           {name: "Rule 1" , value: ' Long balls from own half , Backpassing and kick off glitch are banned in the Tourneys , If Your Opponent Performs these Long balls and you concede a GOAL , You will Report recorded proof in #proves channel and THE FINAL DECISION WILL BE TAKEN BY THE STAFF MEMBERS And THAT SHOULD BE Respected . ONLY IF YOU Concede Goal by that , you can do Long ball but remember you must not score with that'},
            {name: "Rule 2" , value: ' If you find Your Opponent lagging , Quit in first 30 mins else quitting afterwards will straight lead to the score at that time or full time score'}
            
            
