dc4c2795ad849409f47fcb8cc04198dc78659720
Manish gupta <NightmareManish>
2021-05-30 18:55:28 +0530

    Update stop.js

--- music/stop.js
+++ music/stop.js
@@ -1,14 +1,14 @@
 module.exports ={
     name: 'stop',
     Description: 'this is a music command!',
-    execute(client,message,args, Discord){
+    async execute(client,message,args, Discord){
      
         if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
 
-        let queue = await bot.distube.getQueue(message);
+        let queue = await client.distube.getQueue(message);
     
         if(queue) {
-            bot.distube.stop(message)
+            client.distube.stop(message)
     
             message.channel.send('Stopped Music')
         } else if (!queue) {
