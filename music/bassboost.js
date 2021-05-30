e31e9c5b12e61096cccfb461b2d452b659db4133
Manish gupta <NightmareManish>
2021-05-31 02:16:24 +0530

    filter

--- music/bassboost.js
+++ music/bassboost.js
@@ -6,7 +6,19 @@ module.exports ={
         
         if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
         let filter = client.distube.setFilter(message,'bassboost');
-        message.channel.send("Current queue filter: " + (filter || "Off"));
+        const embed = new Discord.MessageEmbed()
+        .setColor('#F0074F')
+        .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
+        .setAuthor('Jarvis' , 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+        .setDescription(`${"Current queue filter: " + (filter || "Off")} `)
+      
+        
+        .setFooter(client.user.username, client.user.displayAvatarURL())
+        .setTimestamp();
+        message.channel.send(embed);
+
+        
+
 
         
 
