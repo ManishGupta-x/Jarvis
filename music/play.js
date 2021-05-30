b45fda3178c97173fb220ab546ee454a8f6f1f79
Manish gupta <NightmareManish>
2021-05-30 18:56:28 +0530

    Update play.js

--- music/play.js
+++ music/play.js
@@ -2,7 +2,7 @@ module.exports ={
     name: 'play',
     Description: 'this is a music command!',
     execute(client,message,args, Discord){
-     
+        if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
         const music = args.join(" ");
 
         client.distube.play(message, music)
