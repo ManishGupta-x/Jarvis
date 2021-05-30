2b5e6eea5b9ac3b13505c22167acb0bc6c24a446
Manish gupta <NightmareManish>
2021-05-31 01:29:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ client.distube
     .on("playSong", (message, queue, song) => {
         const playsong = new Discord.MessageEmbed()
             .setColor('#F0074F')
+            .setAuthor('Jarvis' , 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
             .setTitle('Playing')
             .setDescription(`${song.name} | Requested by: ${song.user} [${song.formattdeDuration}] `)
             
-            .setFooter(`Requested by: ${song.user}`, client.user.displayAvatarURL())
+            .setFooter(`Requested by: ${song.username}`, client.user.displayAvatarURL())
             .setTimestamp();
 
         message.channel.send(playsong)
@@ -27,10 +28,10 @@ client.distube
         const addsong = new Discord.MessageEmbed()
     .setColor('#F0074F')
     .setTitle('Added to Queue')
-   
+    .setAuthor('Jarvis' , 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
     .setDescription(`${song.name} | Requested by: ${song.user} [${song.formattdeDuration}] `)
             
-    .setFooter(`Requested by: ${song.user}`, client.user.displayAvatarURL())
+    .setFooter(`Requested by: ${song.username}`, client.user.displayAvatarURL())
     .setTimestamp();
         
         message.channel.send(addsong)})
