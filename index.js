428883ccc0e3dc759e28db5b25c04ba825bd8ff3
Manish gupta <NightmareManish>
2021-05-31 16:46:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ client.distube
             .setFooter(client.user.username, client.user.displayAvatarURL())
             .setTimestamp();
         let btn3 = new client.disbut.MessageButton()
-            .setStyle('gray')
+            .setStyle('blurpurple')
             .setLabel('Nightcore')
             .setID('Nightcore');
 
