e9ee58e2bc2c5ec9b0b078e71bfe68c84da20920
Manish gupta <NightmareManish>
2021-05-31 11:55:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -166,21 +166,20 @@ client.on('clickButton', async (button) => {
         } else if (!queue) {
             return
         }
-    } else if (button.id === 'Stop') {
+    } else if (button.id === 'BB') {
         await button.defer();
-        button.channel.send('Stopped Music')
-
         if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
-
-        let queue = await client.distube.getQueue(message);
-
-        if (queue) {
-            client.distube.stop(message)
-
-
-        } else if (!queue) {
-            return
-        }
+        let filter = client.distube.setFilter(message,'bassboost');
+        const embed = new Discord.MessageEmbed()
+        .setColor('#F0074F')
+        .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
+        .setAuthor('Jarvis' , 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+        .setDescription(`${"Current queue filter: " + (filter || "Off")} `)
+      
+        
+        .setFooter(client.user.username, client.user.displayAvatarURL())
+        .setTimestamp();
+        message.channel.send(embed);
 
 
     } else if (button.id === 'Queue') {
