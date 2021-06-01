24ca6a507801a1fbe38ef76df104f80f79ca0a34
Manish gupta <NightmareManish>
2021-06-01 11:56:18 +0530

    Update loop.js

--- music/loop.js
+++ music/loop.js
@@ -6,12 +6,12 @@ module.exports ={
         
         if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
         
-        client.distube.setRepeatMode(message, parseInt(args[0]));
+        let filter = client.distube.setRepeatMode(message, parseInt(args[0]));
         const embed = new Discord.MessageEmbed()
         .setColor('#F0074F')
         .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
         .setAuthor('Jarvis' , 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-        .setDescription(`looping`)
+        .setDescription(`${"Loop : " + (filter || "Off")} `)
       
         
         .setFooter(client.user.username, client.user.displayAvatarURL())
