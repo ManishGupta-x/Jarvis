47eae48d8cdd8a725a928c2eb19272871fa87b4d
Manish gupta <NightmareManish>
2021-06-04 00:21:02 +0530

    e

---
+++ commands/calc.js
@@ -0,0 +1,21 @@
+const math = require('math.js');
+module.exports ={
+    name: 'calc',
+    Description: 'this is a  Ping command!',
+    execute(message,args, Discord){
+     if(args[0]) return message.channel.send('Please Input Calculation');
+     let resp;
+     try{
+         resp = math.eval(args.join(' '));
+     }catch(e){
+         return message.channel.send('sorry, please input a valid calculation');
+     }
+     const embed = new Discord.MessageEmbed()
+     .setColor('RANDOM')
+     .setTitle('Calculator') 
+     .addField('Input', `\`\`\`js\n${args.join('')}\`\`\``)           
+     .addField('Output', `\`\`\`js\n${resp}\`\`\``)             
+
+     message.channel.send(embed);
+    }  
+}
