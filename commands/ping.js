c60099150f2da54bc75a052877fea49913e5f505
NightmareManish <manishgupta220402@gmail.com>
2021-06-29 14:43:03 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,7 @@ module.exports ={
       message.reply('Computing...').then((resultMessage) => {
          const ping = resultMessage.createdTimestamp - message.createdTimestamp
    
-         resultMessage.edit(`Latency: ${ping}, API Latency: ${client.ws.ping}`)
+         resultMessage.edit(`Latency: \`${ping}\`, API Latency: \`${client.ws.ping}\``)
        })
 
 
