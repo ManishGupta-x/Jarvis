7ccb03ef1312c592e1af669447195f8ac306d16d
NightmareManish <manishgupta220402@gmail.com>
2021-07-30 23:51:20 +0530

    Update av.js

--- commands/av.js
+++ commands/av.js
@@ -7,7 +7,6 @@ module.exports ={
         let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]) || message.member;
         const userEmbed = new Discord.MessageEmbed()
         .setAuthor(member.user.tag, member.user.displayAvatarURL())
-        .setTimestamp()
         .setColor('RANDOM')
         .setImage(member.user.displayAvatarURL())
         
