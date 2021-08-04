00dbd90f6832455090c2fa29b665c687bf335ecd
NightmareManish <manishgupta220402@gmail.com>
2021-08-04 13:55:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ for (const file of idFiles) {
 
 client.once('ready', () => {
     console.log(' The Jarvis is online!');
-    client.user.setActivity('to Boss 😎', { type:'LISTENING' }).catch(console.error);
+    client.user.setActivity('Boss 😎', { type:'LISTENING' }).catch(console.error);
 
 });
 module.exports.timedcheck = undefined;
