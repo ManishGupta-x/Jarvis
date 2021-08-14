56ad0e383b740bd41e262462589c018f262d350c
NightmareManish <manishgupta220402@gmail.com>
2021-08-14 18:03:02 +0530

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,10 +1,10 @@
 module.exports ={
     name: 'report',
     Description: 'this is a report command!',
-    execute(message,args, Discord){
-     
+    execute(client,message,args, Discord){
+        var server = client.guilds.get(message.guild.id).id;
         const channel = `876076486463987752`;
-        channel.send(`Guild Id : ${message.guild.id} Requesting for restart`)
+        channel.send(`Guild Id : ${server} Requesting for restart`)
 
 
     }  
