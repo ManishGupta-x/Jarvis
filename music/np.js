86fd372c7f6b1ef73349e436f031123d02c703a5
NightmareManish <manishgupta220402@gmail.com>
2021-08-23 20:10:42 +0530

    np

--- music/np.js
+++ music/np.js
@@ -6,9 +6,20 @@ module.exports ={
         if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
 
         let queue = await client.distube.getQueue(message);
-       
-        message.channel.send('Current queue:\n' + queue.songs.map((song, id) =>
-        `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``
-    ).slice(0,1).join("\n"));
+        queue.songs.map((song,id) => {
+            const embed = new Discord.MessageEmbed()
+            .setColor('#F0074F')
+            .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
+            .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+            .setTitle(`Now Playing`)
+            .setDescription(`${song.name}- \`${song.formattedDuration}\``).slice(0,1).join("\n");
+
+
+
+
+        })
+
+      
+
     }  
 } 
