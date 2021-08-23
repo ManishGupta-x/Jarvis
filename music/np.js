cd40d71f5077f31e9461778d285274de98621284
NightmareManish <manishgupta220402@gmail.com>
2021-08-23 20:15:46 +0530

    Update np.js

--- music/np.js
+++ music/np.js
@@ -12,12 +12,12 @@ module.exports ={
             .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
             .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
             .setTitle(`Now Playing`)
-            .setDescription(`${song.name}- \`${song.formattedDuration}\``).slice(0,1).join("\n");
+            .setDescription(`${song.name}- \`${song.formattedDuration}\``)
 
 
 
 
-        })
+        }).slice(0, 1).join("\n");
 
       
 
