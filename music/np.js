a0bc4b295fc44a782d7bad335d987040eb37a7d2
NightmareManish <manishgupta220402@gmail.com>
2021-08-31 21:07:18 +0530

    music

--- music/np.js
+++ music/np.js
@@ -1,4 +1,9 @@
 const disTube = require('distube');
+const wallpapers = ["https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png", "https://cdn.discordapp.com/attachments/730714810614022228/882284561726308372/433536-Klayton-women-science_fiction-planet-Scandroid.png"
+, "https://cdn.discordapp.com/attachments/730714810614022228/882284789145677854/Drum-Instrument-Neon-HD-Wallpapers-Free-Download.png", "https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png",
+"https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png","https://cdn.discordapp.com/attachments/730714810614022228/882284384202395678/neon-wallpaper-2008181520192-scaled.png","https://cdn.discordapp.com/attachments/730714810614022228/882283761868357682/edm-house-music-dj-producer-beatmaker-wallpaper-hd-4k-desktop-6-2048x1080.png"]
+const mu = wallpapers[Math.floor(Math.random() * wallpapers.length)];
+
 module.exports ={
     name: 'np',
     Description: 'this is a music command!',
@@ -9,11 +14,11 @@ module.exports ={
         queue.songs.map((song,id) => {
             const embed = new Discord.MessageEmbed()
             .setColor('#F0074F')
-            .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
+            .setThumbnail(`${mu}`)
             .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
             .setTitle(`Now Playing`)
             .setDescription(`${song.name} | Requested by: ${song.user}\` - ${song.formattedDuration}\` `)
-            .setImage(`${song.setThumbnail}`)
+            
             message.channel.send(embed);
 
 
