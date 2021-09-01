d52a8c322e6f65efcd2a1f7205c8bd10b7b1cfe4
NightmareManish <manishgupta220402@gmail.com>
2021-09-01 12:33:19 +0530

    my squad

---
+++ commands/mysquad.js
@@ -0,0 +1,34 @@
+const formation = ["3-4-3", "4-1-2-3", "5-3-2", "4-1-3-2","4-2-2-2", "4-2-1-3","5-2-1-2","4-2-3-1"]
+const fr =formation[Math. floor(Math.random() * formation.length)];
+const Difficulty = ["1 Cf Must be a goalkeeper", "1 Cb Must be a goalkeeper" , "1 Cb must be Amf Player", "1 Cf must be a Center back","A Gk of rating less than 85", "1 Mid player as goalkeeper"]
+const df = Difficulty[Math.floor(Math.random() * Difficulty.length)];
+const Ims = ["1","2","3","4"]
+const im = Ims[Math.floor(Math.random() *Ims.length)];
+let membertarget = message.guild.members.cache.get(member.id);
+module.exports ={
+    name: 'mysquad',
+    Description: 'this is a role command!',
+    execute(message,args, Discord){
+     
+        let imlegend = im; 
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setThumbnail('https://i.imgur.com/KmTA1Mn.gif')
+        .setTitle(`${client.users.cache.get(membertarget.id).username}'s Squad Requirements`)
+        .setAuthor('Jarvis','https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+        .setDescription(`
+        
+        Formantion : ${fr}\n Max Im's In Whole squad = ${imlegend}\n Max Legends in whole squad = ${6-imlegend}\n Max featured in whole squad = ${im} \n Special Difficulty = ${df}\n Remaining players must be Base players !
+
+        
+        
+        `)
+
+            
+            
+            message.channel.send(newEmbed);
+ 
+
+
+
+    }  }
