1590ea4765439ebd9d195dd7667b09bb6b7a3ec6
NightmareManish <manishgupta220402@gmail.com>
2021-09-07 14:17:18 +0530

    edit

--- commands/fgtourney-deadline.js
+++ commands/fgtourney-deadline.js
@@ -1,7 +1,7 @@
 module.exports ={
     name: 'fgtourney-deadline',
     Description: 'this is a role command!',
-    execute(message,args, Discord){
+    execute(client,message,args, Discord){
      
        message.channel.send('22nd of september,2021 ( Group stage)');
 
--- commands/fgtourney.js
+++ commands/fgtourney.js
@@ -1,7 +1,7 @@
 module.exports ={
     name: 'fgtourney',
     Description: 'Gives fg tourney Info !',
-    execute(message,args,Discord){
+    execute(client,message,args,Discord){
      
        const newEmbed = new Discord.MessageEmbed()
        .setColor('RANDOM')
