fb781286e90062caa49401c91ee33191d6bc9bc2
NightmareManish <manishgupta220402@gmail.com>
2021-09-09 19:50:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const prefix = 'p!';
 const mongoose = require('mongoose');
 const { Client, Intents } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const fetch = require("node-fetch").default;
 const DisTube = require('distube');
