099283c2f7487be04388cafe00b33326d6aae038
NightmareManish <manishgupta220402@gmail.com>
2021-09-11 13:50:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ client.distube
             );
         queue.textChannel.send({ embeds: [playsong], components: [row] }).then( async (client,message,args,Discord) =>{
 
-            const filter = i => i.customId === 'skip' ||i.customId === 'BassBoost'|| i.customId === 'Nightcore' && i.clicker.user.id === `${message.author.id}`;
+            const filter = i => i.customId === 'skip' ||i.customId === 'BassBoost'|| i.customId === 'Nightcore' && i.user.id === `${message.author.id}`;
 
             const collector = interaction.channel.createMessageComponentCollector({ filter, time: 300000 });
             collector.on('collect', async i  => {
