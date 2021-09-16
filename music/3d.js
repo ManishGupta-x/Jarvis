d30e1ff4d30d1360c6c7671da5338a6787862c4f
NightmareManish <manishgupta220402@gmail.com>
2021-09-16 18:31:39 +0530

    Update 3d.js

--- music/3d.js
+++ music/3d.js
@@ -1,17 +1,17 @@
 const disTube = require('distube');
 module.exports ={
-    name: '8d',
+    name: '3d',
     Description: 'this is a music command!',
     execute(client,message,args, Discord){
         
         if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
-        let filter = client.distube.setFilter(message,'8d');
+        let filter = client.distube.setFilter(message,'3d');
 
         const embed = new Discord.MessageEmbed()
         .setColor('#F0074F')
         .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
         .setAuthor('Jarvis' , 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-        .setDescription("3d:`" + (filter ? "On" : "Off") + "`")
+        .setDescription(`${"Current queue filter: " + (filter || "Off")} `)
       
         
         .setFooter(client.user.username, client.user.displayAvatarURL())
