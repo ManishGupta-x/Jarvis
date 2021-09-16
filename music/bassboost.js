6cd5487f32210ca37f721813561818ff436a8b99
NightmareManish <manishgupta220402@gmail.com>
2021-09-16 19:06:51 +0530

    Update bassboost.js

--- music/bassboost.js
+++ music/bassboost.js
@@ -7,7 +7,7 @@ module.exports ={
         if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
         let filter = client.distube.setFilter(message,'bassboost');
         const embed = new Discord.MessageEmbed()
-        .setColor('20B8FF')
+        .setColor('#1BE124')
         .setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/888052510416330762/wp7806858.png')
         .setAuthor('Jarvis' , 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
         .setDescription("BassBoost:`" + (filter ? "On" : "Off") + "`")
