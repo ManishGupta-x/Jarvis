11b456f1bd0f2a74922788a25ca64f2dcd6d52e3
NightmareManish <manishgupta220402@gmail.com>
2021-09-17 23:11:18 +0530

    Update loop.js

--- music/loop.js
+++ music/loop.js
@@ -1,4 +1,5 @@
 const disTube = require('distube');
+const { RepeatMode } = require("distube");
 module.exports ={
     name: 'loop',
     Description: 'this is a music command!',
@@ -6,9 +7,19 @@ module.exports ={
         
         if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
         
-        let mode = client.distube.setRepeatMode(message, parseInt(args[0]));
-
-        mode = mode ? mode == 2 ? "Repeat queue" : "Repeat song" : "Off";
+       
+        let mode;
+        switch(client.distube.setRepeatMode(message, parseInt(args[0]))) {
+            case RepeatMode.DISABLED:
+                mode = "Off";
+                break;
+            case RepeatMode.SONG:
+                mode = "Repeat a song";
+                break;
+            case RepeatMode.QUEUE:
+                mode = "Repeat all queue";
+                break;
+        }
         const embed = new Discord.MessageEmbed()
         .setColor('#F0074F')
         .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
