d8ce17542d4df048a3c5ac2f68ce63acb7225616
NightmareManish <manishgupta220402@gmail.com>
2021-09-17 15:00:30 +0530

    Update music.js

--- music/music.js
+++ music/music.js
@@ -6,9 +6,13 @@ module.exports ={
     Description: 'Gives Commands Info !',
     execute(client,message,args,Discord){
       
-
+      var wallpapers = ["https://cdn.discordapp.com/attachments/730714810614022228/888355120411209738/1792303.png",
+      "https://cdn.discordapp.com/attachments/730714810614022228/888355457704542278/977273-desktop-background-music-1920x1080-for-samsung.png",
+      "https://cdn.discordapp.com/attachments/730714810614022228/888355852082376724/photo-1513829596324-4bb2800c5efb.png","https://cdn.discordapp.com/attachments/730714810614022228/888355302599196692/maxresdefault.png",
+    "https://cdn.discordapp.com/attachments/730714810614022228/888355751276449822/photo-1499415479124-43c32433a620.png"]
+       var mu = Math.floor(Math.random() * wallpapers.length);
        const newEmbed1 = new Discord.MessageEmbed()
-       .setColor('#F0074F')
+       .setColor('RANDOM')
        .setTitle('Command Menu Music')
        .setAuthor('The Jarvis v2.0.0', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
        .setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/848623748050976858/dj-console-mixing-music-with-hands-with-blurred-people-dancing-night-club-party_137321-140.png')
@@ -31,7 +35,7 @@ module.exports ={
         
            )
            .setFooter(client.user.username , client.user.displayAvatarURL() )
-          .setImage('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png');
+          .setImage(`${wallpapers[mu]}`);
 
           
           message.channel.send({embeds: [newEmbed1]});
