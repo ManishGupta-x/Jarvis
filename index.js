36cd0123b512c11050def44bdbe48ad4c0d0d1ff
NightmareManish <manishgupta220402@gmail.com>
2021-09-18 13:52:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ client.distube
             .setTimestamp();
 
         queue.textChannel.send({ embeds: [addsong] });
-    });
-    client.distube.on("searchResult", (message, results) => {
+    })
+   .on("searchResult", (message, results) => {
         message.channel.send(`**Choose an option from below**\n${
             results.map((song, i) => `**${i + 1}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")
         }\n*Enter anything else or wait 60 seconds to cancel*`);
