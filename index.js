65d964441204816f62b1b50a95ea5a00a427bd35
NightmareManish <manishgupta220402@gmail.com>
2021-09-18 14:27:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -195,20 +195,21 @@ client.on('messageCreate', async message => {
 
                     client.commands.get('mysquad').execute(client, message, args, Discord);
                     break;
-                case 'search':
-                    const searchsong = args.join(" ");
-                    if(!searchsong){
-                        message.reply('Pls mention song to search')
-                    }if(searchsong){
+                case 'search': message.reply('Under Construction');
+                //     const searchsong = args.join(" ");
+                //     if(!searchsong){
+                //         message.reply('Pls mention song to search')
+                //     }if(searchsong){
                     
-                   const result = await client.distube.search(searchsong,{limit :15});
+                //    const result = await client.distube.search(searchsong,{limit :15});
                   
-                   message.channel.send(`**Choose an option from below**\n${
-                    result.map((song, i) => `**${i + 1}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")
-                }\n*Enter anything else or wait 60 seconds to cancel*`);
-                   await client.distube.play(message,result[0])
-                    }
-                    break;
+                //    message.channel.send(`**Choose an option from below**\n${
+                //     result.map((song, i) => `**${i + 1}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")
+                // }\n*Enter anything else or wait 60 seconds to cancel*`);
+
+                //    await client.distube.play(message,result[0])
+                //     }
+                //     break;
 
                 case 'play':
                     if (!args[0]) {
