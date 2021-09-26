8c5f4aa65b444cfc2aa12ac359acd9136cb7f1d5
NightmareManish <manishgupta220402@gmail.com>
2021-09-27 01:22:17 +0530

    Update queue.js

--- music/queue.js
+++ music/queue.js
@@ -4,7 +4,10 @@ module.exports ={
     Description: 'this is a music command!',
     async execute(client,message,args, Discord){
         if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
-
+        var wallpapers = ["https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png", "https://cdn.discordapp.com/attachments/730714810614022228/882284561726308372/433536-Klayton-women-science_fiction-planet-Scandroid.png"
+        , "https://cdn.discordapp.com/attachments/730714810614022228/882284789145677854/Drum-Instrument-Neon-HD-Wallpapers-Free-Download.png", "https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png",
+        "https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png","https://cdn.discordapp.com/attachments/730714810614022228/882284384202395678/neon-wallpaper-2008181520192-scaled.png","https://cdn.discordapp.com/attachments/730714810614022228/882283761868357682/edm-house-music-dj-producer-beatmaker-wallpaper-hd-4k-desktop-6-2048x1080.png"]
+        var mu = wallpapers[Math.floor(Math.random() * wallpapers.length)];
         let queue = await client.distube.getQueue(message);
         if(!queue){
           message.reply('No queue Available');
@@ -18,12 +21,12 @@ module.exports ={
             
             const embed9 = new Discord.MessageEmbed()
             .setColor('RANDOM')
-            .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
+            .setThumbnail(`${mu}`)
             .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-            .setDescription(`**Current Queue**\n${queue.songs.map((song, id) => `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n\n${status(queue)}`)
+            .setDescription(`**Current Queue**\n\n${queue.songs.map((song, id) => `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n\n${status(queue)}`)
 
 
-            .setFooter(`Total Queue Time : \`${queue.formattedDuration}\``)
+            .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
             .setTimestamp();
         message.channel.send({ embeds: [embed9] });
             
