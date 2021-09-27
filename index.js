a6cf7a5ed2f7ec42503e5be712fcda0b4b4a9262
NightmareManish <manishgupta220402@gmail.com>
2021-09-27 12:28:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -221,6 +221,13 @@ client.on('messageCreate', async message => {
                     break;
                 case 'search':
                     if (!message.member.voice.channel) return message.channel.send({ content: 'You must be in a voice channel to use this command.' });
+                  
+                    var wallpapers = ["https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png", "https://cdn.discordapp.com/attachments/730714810614022228/882284561726308372/433536-Klayton-women-science_fiction-planet-Scandroid.png"
+                    , "https://cdn.discordapp.com/attachments/730714810614022228/882284789145677854/Drum-Instrument-Neon-HD-Wallpapers-Free-Download.png", "https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png",
+                    "https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png","https://cdn.discordapp.com/attachments/730714810614022228/882284384202395678/neon-wallpaper-2008181520192-scaled.png","https://cdn.discordapp.com/attachments/730714810614022228/882283761868357682/edm-house-music-dj-producer-beatmaker-wallpaper-hd-4k-desktop-6-2048x1080.png"]
+                    var mu = wallpapers[Math.floor(Math.random() * wallpapers.length)];
+                  
+                  
                     const searchsong = args.join(" ");
                     if (!searchsong) {
                         message.reply('Pls mention song to search')
