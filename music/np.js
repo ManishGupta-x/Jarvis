aa1dcf79493bd16ec443dec5543e60bda80369dc
NightmareManish <manishgupta220402@gmail.com>
2021-09-28 11:47:30 +0530

    Update np.js

--- music/np.js
+++ music/np.js
@@ -56,7 +56,7 @@ module.exports ={
                 .setThumbnail(`${mu}`)
                 .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
                 .setTitle(`Now Playing`)
-                .setDescription(`${song.name} | Requested by: ${song.user} || at \` ${queue.formattedCurrentTime}\`/ \`${song.formattedDuration}\` `)
+                .setDescription(`${song.name} | Requested by: ${song.user} || at \` ${queue.formattedCurrentTime}\`/ \`${song.formattedDuration}\` \n ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ `)
                 .addField("Upcoming Song :", `${upcomingsong.name} | Requested by: ${upcomingsong.user}\` || ${upcomingsong.formattedDuration}\`\n\n${status(queue)}`)
                 .setFooter(client.user.username, client.user.displayAvatarURL())
                 .setTimestamp();
