24cfe130e1e26b20b62618c10e357449d698250e
NightmareManish <manishgupta220402@gmail.com>
2021-09-30 14:25:59 +0530

    Update np.js

--- music/np.js
+++ music/np.js
@@ -1,20 +1,7 @@
 const disTube = require('distube');
 const { MessageActionRow, MessageButton } = require('discord.js');
 
- function createBar(player) {
-    let size = 30;
-    let line = "▬";
-    let slider = "🔘";
-    
-    if (!player.queue.current) return `${slider}${line.repeat(size - 1)}]`;
-    let current = player.queue.current.duration !== 0 ? player.position : player.queue.current.duration;
-    let total = player.queue.current.duration;
-    let bar = current > total ? [line.repeat(size / 2 * 2), (current / total) * 100] : [line.repeat(Math.round(size / 2 * (current / total))).replace(/.$/, slider) + line.repeat(size - Math.round(size * (current / total)) + 1), current / total];
-   
-    if (!String(bar).includes(slider)) return `${slider}${line.repeat(size - 1)}`;
-    return `${bar[0]}`;
-
-  }
+ 
 
 module.exports ={
     name: 'np',
@@ -26,6 +13,20 @@ module.exports ={
         "https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png","https://cdn.discordapp.com/attachments/730714810614022228/882284384202395678/neon-wallpaper-2008181520192-scaled.png","https://cdn.discordapp.com/attachments/730714810614022228/882283761868357682/edm-house-music-dj-producer-beatmaker-wallpaper-hd-4k-desktop-6-2048x1080.png"]
         var mu = wallpapers[Math.floor(Math.random() * wallpapers.length)];
         let queue = await client.distube.getQueue(message);
+        function createBar(player) {
+            let size = 30;
+            let line = "▬";
+            let slider = "🔘";
+            
+            if (!player.queue.current) return `${slider}${line.repeat(size - 1)}]`;
+            let current = player.queue.current.duration !== 0 ? player.position : player.queue.current.duration;
+            let total = player.queue.current.duration;
+            let bar = current > total ? [line.repeat(size / 2 * 2), (current / total) * 100] : [line.repeat(Math.round(size / 2 * (current / total))).replace(/.$/, slider) + line.repeat(size - Math.round(size * (current / total)) + 1), current / total];
+           
+            if (!String(bar).includes(slider)) return `${slider}${line.repeat(size - 1)}`;
+            return `${bar[0]}`;
+        
+          }
         if(!queue){
             message.reply('No Song in Queue !')
         }else{
