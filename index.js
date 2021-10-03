99fb2c60e212f753158540abb60a5efe17894e7c
NightmareManish <manishgupta220402@gmail.com>
2021-10-03 22:45:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -147,19 +147,19 @@ for (const file of idFiles) {
     client.id.set(command.name, command);
 }
 
-// client.on('messageDelete', async (client, message) => {
+client.on('messageDelete', async message => {
 
-//     if (message.author.bot) return;
-//     client.snipes.set(message.channel.id, {
-//         content: message.content,
-//         author: message.author.tag,
-//         member: message.member,
-//         image: message.attachments.first() ? message.attachments.first().proxyURL : null
+    if (message.author.bot) return;
+    client.snipes.set(message.channel.id, {
+        content: message.content,
+        author: message.author.tag,
+        member: message.member,
+        image: message.attachments.first() ? message.attachments.first().proxyURL : null
 
 
 
-//     })
-// })
+    })
+})
 
 client.once('ready', () => {
     console.log('Jarvis is online!');
@@ -223,17 +223,17 @@ client.on('messageCreate', async message => {
                 case 'ping':
                     client.commands.get('ping').execute(client, message, args, Discord);
                     break;
-                // case 'snipe':
+                case 'snipe':
 
-                //     const msg = client.snipes.get(message.channel.id)
-                //     const embed = new Discord.MessageEmbed()
+                    const msg = client.snipes.get(message.channel.id)
+                    const embed = new Discord.MessageEmbed()
                        
-                //         .setDescription(msg.content)
-                //         .setFooter('Get Sniped lol')
-                //         .setTimestamp();
-                //     message.channel.send({embeds : [embed]});
+                        .setDescription(msg.content)
+                        .setFooter('Get Sniped lol')
+                        .setTimestamp();
+                    message.channel.send({embeds : [embed]});
 
-                //     break;
+                    break;
                 case 'bar':
                     //   async function createbar(){
                     //     let size = 30;
