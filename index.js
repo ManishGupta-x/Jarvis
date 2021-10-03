83f1908077103f3a3c5f1f8e7890b3f4d6da8fe9
NightmareManish <manishgupta220402@gmail.com>
2021-10-03 21:33:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -227,7 +227,7 @@ client.on('messageCreate', async message => {
 
                     const msg = client.snipes.get(message.channel.id)
                     const embed = new Discord.MessageEmbed()
-                        .setAuthor(msg.author, msg.member.user.displayAvatarURL())
+                        .setAuthor(msg.member.user.displayAvatarURL())
                         .setDescription(msg.content)
                         .setFooter('Get Sniped lol')
                         .setTimestamp();
