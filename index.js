fb56c531c0fa48430b5cc5eaec6854d14ceeb2a7
NightmareManish <manishgupta220402@gmail.com>
2021-10-03 22:53:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -681,7 +681,19 @@ client.on('messageCreate', async message => {
                     .setDescription(msg.content)
                     .setFooter('Caught :)')
                     .setTimestamp();
-                message.channel.send({embeds : [embed]});
+                message.channel.send({embeds : [embed]}).catch(error => {
+
+                    const embed5 = new Discord.MessageEmbed()
+                        .setColor('RANDOM')
+                        .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
+                        .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+                        .setDescription(`Message could not be loaded !`)
+
+
+                        .setFooter(client.user.username, client.user.displayAvatarURL())
+                        .setTimestamp();
+                    message.channel.send({ embeds: [embed5] });
+                });;
                 break;
             }
         }
