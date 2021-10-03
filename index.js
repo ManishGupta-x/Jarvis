3f6ec991d92f46a6db3321f08e9aea14c414f14c
NightmareManish <manishgupta220402@gmail.com>
2021-10-03 23:12:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ client.on('messageCreate', async message => {
                     break;
                 case 'snipe':
 
-                   
+
 
                     break;
                 case 'bar':
@@ -674,48 +674,58 @@ client.on('messageCreate', async message => {
                     message.reply("Deactivated Boss!!")
                     d = 0;
                 } break;
-                case "jarvis snipe" :  const msg = client.snipes.get(message.channel.id)
+                case "jarvis snipe": const msg = client.snipes.get(message.channel.id)
+                if (!msg.author) {
+                    message.reply(`Nothing to snipe!`);
+                    return;
+                }
                 const embed = new Discord.MessageEmbed()
-                    .setColor('RANDOM')
-                    .setAuthor(msg.author, client.user.displayAvatarURL())
-                    .setDescription(msg.content)
-                    .setFooter('Caught :)')
-                    .setTimestamp();
-                message.channel.send({embeds : [embed]}).catch(error => {
-
-                    const embed5 = new Discord.MessageEmbed()
                         .setColor('RANDOM')
-                        .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
-                        .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-                        .setDescription(`Message could not be loaded !`)
-
-
-                        .setFooter(client.user.username, client.user.displayAvatarURL())
+                        .setAuthor(msg.author, client.user.displayAvatarURL())
+                        .setDescription(msg.content)
+                        .setFooter('Caught :)')
                         .setTimestamp();
-                    message.channel.send({ embeds: [embed5] });
-                });;
-                break;
-                case "snipe" :  const msg1 = client.snipes.get(message.channel.id)
-                const embed17 = new Discord.MessageEmbed()
-                    .setColor('RANDOM')
-                    .setAuthor(msg1.author, client.user.displayAvatarURL())
-                    .setDescription(msg1.content)
-                    .setFooter('Caught :)')
-                    .setTimestamp();
-                message.channel.send({embeds : [embed17]}).catch(error => {
+                    message.channel.send({ embeds: [embed] }).catch(error => {
 
-                    const embed8 = new Discord.MessageEmbed()
+                        const embed5 = new Discord.MessageEmbed()
+                            .setColor('RANDOM')
+                            .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
+                            .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+                            .setDescription(`Message could not be loaded !`)
+
+
+                            .setFooter(client.user.username, client.user.displayAvatarURL())
+                            .setTimestamp();
+                        message.channel.send({ embeds: [embed5] });
+                    });;
+                    break;
+                case "snipe": const msg1 = client.snipes.get(message.channel.id)
+                    if (!msg1.author) {
+                        message.reply(`Nothing to snipe!`);
+                        return;
+                    }
+
+                    ;
+                    const embed17 = new Discord.MessageEmbed()
                         .setColor('RANDOM')
-                        .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
-                        .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-                        .setDescription(`Message could not be loaded !`)
-
-
-                        .setFooter(client.user.username, client.user.displayAvatarURL())
+                        .setAuthor(msg1.author, client.user.displayAvatarURL())
+                        .setDescription(msg1.content)
+                        .setFooter('Caught :)')
                         .setTimestamp();
-                    message.channel.send({ embeds: [embed8] });
-                });;
-                break;
+                    message.channel.send({ embeds: [embed17] }).catch(error => {
+
+                        const embed8 = new Discord.MessageEmbed()
+                            .setColor('RANDOM')
+                            .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
+                            .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+                            .setDescription(`Message could not be loaded !`)
+
+
+                            .setFooter(client.user.username, client.user.displayAvatarURL())
+                            .setTimestamp();
+                        message.channel.send({ embeds: [embed8] });
+                    });;
+                    break;
             }
         }
     }
