2afdea9654beffd4bfe3b7b1a3e68d64c74cf14b
NightmareManish <manishgupta220402@gmail.com>
2021-10-06 12:19:59 +0530

    Update lyrics.js

--- music/lyrics.js
+++ music/lyrics.js
@@ -13,75 +13,76 @@ module.exports = {
 
     //Get the current Queue
     let queue = await client.distube.getQueue(message);
-    const song = queue.songs[0];
+    if (!queue) { const song = queue.songs[0] };
     //If no Queue Error
-    if (!queue) { message.reply('No song Playing') }
-if(args[0]){
+    if (!args[0]) { if (!queue) { message.reply('No song Playing') } }
+    if (args[0]) {
 
-  const searchsong = args.join(" ");
-  var lyrics1 = await lyricsFinder("", searchsong)
+      const searchsong = args.join(" ");
+      var lyrics1 = await lyricsFinder("", searchsong)
 
-  if (!lyrics1) {
-    lyrics1 = await solenolyrics.requestLyricsFor(searchsong)
+      if (!lyrics1) {
+        lyrics1 = await solenolyrics.requestLyricsFor(searchsong)
 
-  }
-  if (!lyrics1) {
-    lyrics1 = `No lyrics found for ${song.name}.`;
-  }
-
-
-  let y1 = Util.splitMessage(lyrics1, { maxLength: 3500 })
-  if (lyrics1) {
-    let lyricsEmbed1 = new Discord.MessageEmbed()
-      .setTitle("📑 Lyrics")
-      .setDescription(`**${searchsong}}**\n\n${y1[0]}`)
-      .setColor('RANDOM')
-    let lyricsEmbed3 = new Discord.MessageEmbed()
-      .setTitle("📑 Lyrics")
-      .setDescription(`${y1[1]}`)
-      .setColor('RANDOM')
+      }
+      if (!lyrics1) {
+        lyrics1 = `No lyrics found for ${song.name}.`;
+      }
 
 
-    message.channel.send({ embeds: [lyricsEmbed1] })
+      let y1 = Util.splitMessage(lyrics1, { maxLength: 3500 })
+      if (lyrics1) {
+        let lyricsEmbed1 = new Discord.MessageEmbed()
+          .setTitle("📑 Lyrics")
+          .setDescription(`**${searchsong}}**\n\n${y1[0]}`)
+          .setColor('RANDOM')
+        let lyricsEmbed3 = new Discord.MessageEmbed()
+          .setTitle("📑 Lyrics")
+          .setDescription(`${y1[1]}`)
+          .setColor('RANDOM')
 
-    if (y1[1]) {
-      await message.channel.send({ embeds: [lyricsEmbed3] })
-    }
-  }
 
-}
+        message.channel.send({ embeds: [lyricsEmbed1] })
 
-if(!args[0]){
-    var lyrics = await lyricsFinder("", `${song.name}`)
-
-    if (!lyrics) {
-      lyrics = await solenolyrics.requestLyricsFor(`${song.name}`)
+        if (y1[1]) {
+          await message.channel.send({ embeds: [lyricsEmbed3] })
+        }
+      }
 
     }
-    if (!lyrics) {
-      lyrics = `No lyrics found for ${song.name}.`;
-    }
 
-    let y = Util.splitMessage(lyrics, { maxLength: 3500 })
-    if (lyrics) {
-      let lyricsEmbed = new Discord.MessageEmbed()
-        .setTitle("📑 Lyrics")
-        .setDescription(`**${song.name}**\n\n${y[0]}`)
-        .setColor('RANDOM')
-      let lyricsEmbed2 = new Discord.MessageEmbed()
-        .setTitle("📑 Lyrics")
-        .setDescription(`${y[1]}`)
-        .setColor('RANDOM')
+    if (!args[0]) {
+      var lyrics = await lyricsFinder("", `${song.name}`)
+
+      if (!lyrics) {
+        lyrics = await solenolyrics.requestLyricsFor(`${song.name}`)
+
+      }
+      if (!lyrics) {
+        lyrics = `No lyrics found for ${song.name}.`;
+      }
+
+      let y = Util.splitMessage(lyrics, { maxLength: 3500 })
+      if (lyrics) {
+        let lyricsEmbed = new Discord.MessageEmbed()
+          .setTitle("📑 Lyrics")
+          .setDescription(`**${song.name}**\n\n${y[0]}`)
+          .setColor('RANDOM')
+        let lyricsEmbed2 = new Discord.MessageEmbed()
+          .setTitle("📑 Lyrics")
+          .setDescription(`${y[1]}`)
+          .setColor('RANDOM')
 
 
-      message.channel.send({ embeds: [lyricsEmbed] })
+        message.channel.send({ embeds: [lyricsEmbed] })
 
-      if (y[1]) {
-        await message.channel.send({ embeds: [lyricsEmbed2] })
+        if (y[1]) {
+          await message.channel.send({ embeds: [lyricsEmbed2] })
+        }
       }
     }
-  }}
+  }
 
 
-  
+
 };
