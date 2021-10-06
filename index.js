b8d2fa2f27438de6fe8bc6ffcea8ea474ad70d42
NightmareManish <manishgupta220402@gmail.com>
2021-10-06 11:55:56 +0530

    lyrics

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ client.on('messageDelete', async message => {
     if (message?.author?.bot) return;
     client.snipes.set(message.channel.id, {
         content: message.content,
-        author: message.author.tag,
+        author: message?.author?.tag,
         member: message.member,
         image: message.attachments.first() ? message.attachments.first().proxyURL : null
 
--- music/lyrics.js
+++ music/lyrics.js
@@ -1,48 +1,51 @@
 
-const solenolyrics= require("solenolyrics"); 
+const solenolyrics = require("solenolyrics");
 const lyricsFinder = require("lyrics-finder")
 const disTube = require('distube');
 const { MessageEmbed, Util } = require("discord.js")
 module.exports = {
   name: "lyrics",
   Description: "Get lyrics for the currently playing song",
-  
+
 
   async execute(client, message, args, Discord) {
-    
-    
+
+
     //Get the current Queue
     let queue = await client.distube.getQueue(message);
     const song = queue.songs[0];
     //If no Queue Error
-    if (!queue ) { message.reply('No song Playing')}      
-     
-  
-     
-     var lyrics =  await lyricsFinder("",`${song.name}`)
-    
-      if (!lyrics) 
-    {  lyrics = await solenolyrics.requestLyricsFor(`${song.name}`)
-      
-      }
-      if(!lyrics){
-      lyrics = `No lyrics found for ${song.name}.`;}
-    
-      let y = Util.splitMessage(lyrics, { maxLength: 3500 })
-if(lyrics){
-    let lyricsEmbed = new Discord.MessageEmbed()
-      .setTitle("📑 Lyrics")
-      .setDescription(`**${song.name}**\n\n${y[0]}`)
-      .setColor('RANDOM')
-      let lyricsEmbed2 = new Discord.MessageEmbed()
-      .setTitle("📑 Lyrics")
-      .setDescription(`${y[1]}`)
-      .setColor('RANDOM')
-    
-     
-    message.channel.send({ embeds : [lyricsEmbed]})
+    if (!queue) { message.reply('No song Playing') }
 
-    if(y[1]){
-    await   message.channel.send({ embeds : [lyricsEmbed2]})}
-  }}
+
+
+    var lyrics = await lyricsFinder("", `${song.name}`)
+
+    if (!lyrics) {
+      lyrics = await solenolyrics.requestLyricsFor(`${song.name}`)
+
+    }
+    if (!lyrics) {
+      lyrics = `No lyrics found for ${song.name}.`;
+    }
+
+    let y = Util.splitMessage(lyrics, { maxLength: 3500 })
+    if (lyrics) {
+      let lyricsEmbed = new Discord.MessageEmbed()
+        .setTitle("📑 Lyrics")
+        .setDescription(`**${song.name}**\n\n${y[0]}`)
+        .setColor('RANDOM')
+      let lyricsEmbed2 = new Discord.MessageEmbed()
+        .setTitle("📑 Lyrics")
+        .setDescription(`${y[1]}`)
+        .setColor('RANDOM')
+
+
+      message.channel.send({ embeds: [lyricsEmbed] })
+
+      if (y[1]) {
+        await message.channel.send({ embeds: [lyricsEmbed2] })
+      }
+    }
+  }
 };
