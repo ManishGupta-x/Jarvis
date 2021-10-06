4dd1474ad07463bb171415ed429574ff45be7257
NightmareManish <manishgupta220402@gmail.com>
2021-10-06 12:07:05 +0530

    Update lyrics.js

--- music/lyrics.js
+++ music/lyrics.js
@@ -17,33 +17,33 @@ module.exports = {
     //If no Queue Error
     if (!queue) { message.reply('No song Playing') }
 if(args[0]){
-  var lyrics = await lyricsFinder("", args[0])
+  var lyrics1 = await lyricsFinder("", args[0])
 
-  if (!lyrics) {
-    lyrics = await solenolyrics.requestLyricsFor(args[0])
+  if (!lyrics1) {
+    lyrics1 = await solenolyrics.requestLyricsFor(args[0])
 
   }
-  if (!lyrics) {
-    lyrics = `No lyrics found for ${song.name}.`;
+  if (!lyrics1) {
+    lyrics1 = `No lyrics found for ${song.name}.`;
   }
 
 
-  let y = Util.splitMessage(lyrics, { maxLength: 3500 })
-  if (lyrics) {
-    let lyricsEmbed = new Discord.MessageEmbed()
+  let y1 = Util.splitMessage(lyrics1, { maxLength: 3500 })
+  if (lyrics1) {
+    let lyricsEmbed1 = new Discord.MessageEmbed()
       .setTitle("📑 Lyrics")
-      .setDescription(`**${song.name}**\n\n${y[0]}`)
+      .setDescription(`**${song.name}**\n\n${y1[0]}`)
       .setColor('RANDOM')
-    let lyricsEmbed2 = new Discord.MessageEmbed()
+    let lyricsEmbed3 = new Discord.MessageEmbed()
       .setTitle("📑 Lyrics")
-      .setDescription(`${y[1]}`)
+      .setDescription(`${y1[1]}`)
       .setColor('RANDOM')
 
 
-    message.channel.send({ embeds: [lyricsEmbed] })
+    message.channel.send({ embeds: [lyricsEmbed1] })
 
-    if (y[1]) {
-      await message.channel.send({ embeds: [lyricsEmbed2] })
+    if (y1[1]) {
+      await message.channel.send({ embeds: [lyricsEmbed3] })
     }
   }
 
