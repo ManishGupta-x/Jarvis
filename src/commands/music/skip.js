f1709555732cc5516c6ca91d8c7dd1387fc7a478
NightmareManish <manishgupta220402@gmail.com>
2021-10-07 12:32:04 +0530

    Update skip.js

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -3,19 +3,19 @@ const disTube = require('distube');
 
 const Discord = require("discord.js")
 
-module.exports.run = async (client, message, args,Discord) => {
+module.exports.run = async (client, message, args, Discord) => {
     if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
-           
+
     let queue = await client.distube.getQueue(message);
-    if(!queue){
+    if (!queue) {
         message.reply('No Song in Queue !')
-    }else if(queue.songs.length <= 1 && !queue.autoplay) {
+    } else if (queue.songs.length <= 1 && !queue.autoplay) {
         message.reply(" No Upcoming Song")
-       
-    } else if(queue) {
+
+    } else if (queue) {
         client.distube.skip(message)
 
-        message.channel.send({content: 'Skipped'})
+        message.channel.send({ content: 'Skipped' })
     };
 }
 
