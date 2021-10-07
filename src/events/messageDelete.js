adae5733eca6dc0c9d206eb31d04c146e9ccc524
NightmareManish <manishgupta220402@gmail.com>
2021-10-07 15:44:01 +0530

    Update messageDelete.js

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -3,10 +3,10 @@ module.exports = (client,message) => {
 
         return;
     }
-    if (message.author.bot) return;
+    if (message?.author?.bot) return;
     client.snipes.set(message.channel.id, {
         content: message.content,
-        author: message.author.tag,
+        author: message?.author?.tag,
         member: message.member,
         image: message.attachments.first() ? message.attachments.first().proxyURL : null
 
