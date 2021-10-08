1ba49c6573a029f40085dc3533135b1c4c2e97c3
NightmareManish <manishgupta220402@gmail.com>
2021-10-08 16:48:15 +0530

    Update mytasks.js

--- src/commands/misc/mytasks.js
+++ src/commands/misc/mytasks.js
@@ -17,9 +17,10 @@ module.exports.run = async (client, message, args, Discord) => {
 
         const embed1 = new Discord.MessageEmbed()
             .setColor('RANDOM')
+            .setTitle(" Your Todo List ")
             .setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/895927613292421140/6-things-to-do-list.png')
             .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-            .setDescription(`\n **__Task No__**` + "         " + `**__Task__**        **__Status__** \n\n`+ data.list.map((Task, id, list) => ` **${id + 1}**.    \`${list[id].Task}\` \t  \`${list[id].status}\``).join('\n'))
+            .setDescription(`\n\n\n\n **__Task No__**` + "         " + `**__Task__**        **__Status__** \n\n`+ data.list.map((Task, id, list) => ` **${id + 1}**.    \`${list[id].Task}\` \t  \`${list[id].status}\``).join('\n'))
 
 
             .setFooter(`Type p!todo [task] to add task`, client.user.displayAvatarURL())
