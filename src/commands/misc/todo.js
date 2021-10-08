f60fe8a1ed38b795983014fb2efe6268e798e7a3
NightmareManish <manishgupta220402@gmail.com>
2021-10-08 13:14:54 +0530

    Update todo.js

--- src/commands/misc/todo.js
+++ src/commands/misc/todo.js
@@ -35,15 +35,13 @@ module.exports.run = async (client, message, args, Discord) => {
         });
         data.save();
         message.channel.send({ content: `Yours task \`${task}\` has been Added!` })
-        message.channel.send({content : data.list.map( (Task,id,list) => ` **${id + 1}**  ${list.Task}`).join('\n')})
+        message.channel.send({content : data.list.map( (Task,id,list) => ` **${id + 1}**  ${list[id]}`).join('\n')})
        
     } else if (!data) {
         let newData = new list({
 
             UserID: message.author.id,
-            list: [{
-
-                User: message.author.id,
+            list: [{            
                 Task: task,
             },],
         });
