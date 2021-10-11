fd385d4921699b9160deb56c42829eda4d0df5c9
NightmareManish <manishgupta220402@gmail.com>
2021-10-11 12:57:23 +0530

    Update help.js

--- src/commands/misc/help.js
+++ src/commands/misc/help.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
-const {MessageActionRow, MessageSelectMenu} = require('discord.js')
-module.exports.run = async (client, message, args,Discord) => {
+const { MessageActionRow, MessageSelectMenu } = require('discord.js')
+module.exports.run = async (client, message, args, Discord) => {
 
 
 
@@ -8,53 +8,57 @@ module.exports.run = async (client, message, args,Discord) => {
 
 
 
-    
+
     const row = new Discord.MessageActionRow()
-    .addComponents(
+        .addComponents(
 
-        new MessageSelectMenu()
-        .setCustomId('help')
-        .setPlaceholder('Choose Help Module To View')
-        .addOptions([
-                       {
+            new MessageSelectMenu()
+                .setCustomId('help')
+                .setPlaceholder('Choose Help Module To View')
+                .addOptions([
+                    {
 
-                        label : 'Music',
-                        value : 'View Music Help Menu',
-                        emoji : '🎶'
-                       },
-                       {
+                        label: 'Music',
+                        value: 'View Music Help Menu',
+                        emoji: '🎶'
+                    }
 
-                        label : 'Todo',
-                        value : 'View Todo Help Menu',
-                        emoji : '✍️'
-                       }
 
-        ])
-    )
+                ],[
+                    {
+
+                        label: 'Todo',
+                        value: 'View Todo Help Menu',
+                        emoji: '✍️'
+
+                    }
+                ]
+                )
+        )
     const embed = new Discord.MessageEmbed()
-    .setColor('RANDOM')
-    .setThumbnail('https://cdn.discordapp.com/attachments/794240262972243979/897009795146612782/561283.png')
-    .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-     
-    .setFooter(client.user.username, client.user.displayAvatarURL())
-    .setTimestamp();
+        .setColor('RANDOM')
+        .setThumbnail('https://cdn.discordapp.com/attachments/794240262972243979/897009795146612782/561283.png')
+        .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+
+        .setFooter(client.user.username, client.user.displayAvatarURL())
+        .setTimestamp();
 
 
 
-    message.channel.send({ embeds: [embed] , components : [row] });
-    const filter = (interaction) => interaction.isSelectMenu() &&  interaction.user.id == message.author.id;
-    const collector = message.channel.createMessageComponentCollector({ filter, time: 100000, max : 2 });
+    message.channel.send({ embeds: [embed], components: [row] });
+    const filter = (interaction) => interaction.isSelectMenu() && interaction.user.id == message.author.id;
+    const collector = message.channel.createMessageComponentCollector({ filter, time: 100000, max: 2 });
     collector.on('collect', async collected => {
-        
-if(collected.values[0]){
 
-      const commandfile = client.commands.get('music')
-      await commandfile.run(client, message, args, Discord)
-}else if(collected.values[1]){
+        if (collected.values[0]) {
 
-    const commandfile = client.commands.get('helptodo')
-    await commandfile.run(client, message, args, Discord)
-}
+            const commandfile = client.commands.get('music')
+            await commandfile.run(client, message, args, Discord)
+        } else if (collected.values[1]) {
+
+            const commandfile = client.commands.get('helptodo')
+            await commandfile.run(client, message, args, Discord)
+        }
     })
 
 }
