d989f114d91e248499297a9b2db4b3c2d7daccc6
NightmareManish <manishgupta220402@gmail.com>
2021-10-11 19:52:32 +0530

    Update calculate.js

--- src/commands/misc/calculate.js
+++ src/commands/misc/calculate.js
@@ -3,18 +3,22 @@ const math = require('mathjs')
 module.exports.run = async (client, message, args,Discord) => {
     let task = args.slice(0).join(" ");
 
+try {
 
     const embed = new Discord.MessageEmbed()
 .setColor('RANDOM')
 .setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/897101009132871690/calculator-pencil-pen-paper.png')
 .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-.addField("**__Question__**", task)
-.addField("**__Solution__**", math.evaluate(task))
+.addField("**__Question__**", args.join(" "))
+.addField("**__Solution__**", math.evaluate(args.join(" ")))
 .setFooter(client.user.username, client.user.displayAvatarURL())
 .setTimestamp();
 message.channel.send({ embeds: [embed] });
  
+}catch(err){
 
+    message.channel.send(' Invalid Question -_-')
+}
 
 
 }
