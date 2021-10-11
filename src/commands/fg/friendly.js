bf1ac0891587143a5c0ebe35586a63b8a6f335fd
NightmareManish <manishgupta220402@gmail.com>
2021-10-11 19:11:30 +0530

    Update friendly.js

--- src/commands/fg/friendly.js
+++ src/commands/fg/friendly.js
@@ -9,78 +9,85 @@ var x = 0;
 
 const Data = require('../../models/data');
 const usedCommand = new Set();
-module.exports = {
-    name: 'friendly',
-    Description: 'this is a  Friendly  command!',
-    async execute(message, args, Discord, client) {
 
-        if (usedCommand.has(message.author.id)) {
 
-            message.channel.send('https://tenor.com/view/slow-down-a-little-bit-anthony-mennella-culter35-slow-down-dont-rush-gif-17969625')
-            message.reply('The command has a Cooldown of 30 mins')
+const Discord = require("discord.js")
+
+module.exports.run = async (client, message, args,Discord) => {
+    if (usedCommand.has(message.author.id)) {
+
+        message.channel.send('https://tenor.com/view/slow-down-a-little-bit-anthony-mennella-culter35-slow-down-dont-rush-gif-17969625')
+        message.reply('The command has a Cooldown of 30 mins')
+    }
+    else {
+        const channel = '885897626753593364';
+      
+        let member = message.member
+        let membertarget = message.guild.members.cache.get(member.id);
+        const newEmbed = new Discord.MessageEmbed()
+
+            .setAuthor('Freak Gamer')
+            .setColor('00ff74')
+            .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/791728647005536296/1601319291_559_Soccer-Icons-Messi-and-Ronaldo-Make-History-in-the-25th.png')
+            .setTitle(`Someone is Challenging `)
+            .setDescription(`\n <@${membertarget.id}> is Challenging For friendly ! React Below on 🤝 to Accept his Challenge! Get <@${membertarget.id}> Id by reacting to 👨‍💻`)
+            .setFooter(' Type p!fping If you dont want pings! ')
+            const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('Accept')
+                    .setLabel('🏃‍♂️ Accept Chal;enge')
+                    .setStyle('DANGER'),
+
+                new MessageButton()
+                    .setCustomId('id')
+                    .setLabel('👨‍💻 Get Challenger Id')
+                    .setStyle('SUCCESS'),
+              
+               
+            );
+
+        if (message.channel.id == channel) {
+
+                    message.channel.send({embeds : [newEmbed] , components: [row]})
+              
+            const filter = (interaction) => interaction.isButton() && interaction.user.id != message.author.id && interaction.customId === 'Accept'  || interaction.customId === 'id' ;
+            const collector = message.channel.createMessageComponentCollector({ filter, time: 9000000 });
+            
+
+            collector.on('collect', async collected => {
+
+
+                if (collected.customId === 'Accept'){
+                
+                const  user = interaction.user.id;
+                await collected.channel.send({ contents: `${user.id}`})
+                }
+                else if(collected.customId == 'id'){
+
+
+                }
+
+            })
+           
         }
         else {
-            const channel = '885897626753593364';
-          
-            let member = message.member
-            let membertarget = message.guild.members.cache.get(member.id);
-            const newEmbed = new Discord.MessageEmbed()
 
-                .setAuthor('Freak Gamer')
-                .setColor('00ff74')
-                .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/791728647005536296/1601319291_559_Soccer-Icons-Messi-and-Ronaldo-Make-History-in-the-25th.png')
-                .setTitle(`Someone is Challenging `)
-                .setDescription(`\n <@${membertarget.id}> is Challenging For friendly ! React Below on 🤝 to Accept his Challenge! Get <@${membertarget.id}> Id by reacting to 👨‍💻`)
-                .setFooter(' Type p!fping If you dont want pings! ')
-                const row = new MessageActionRow()
-                .addComponents(
-                    new MessageButton()
-                        .setCustomId('Accept')
-                        .setLabel('🏃‍♂️ Accept Chal;enge')
-                        .setStyle('DANGER'),
-    
-                    new MessageButton()
-                        .setCustomId('id')
-                        .setLabel('👨‍💻 Get Challenger Id')
-                        .setStyle('SUCCESS'),
-                  
-                   
-                );
-
-            if (message.channel.id == channel) {
-
-                        message.channel.send({embeds : [newEmbed] , components: [row]})
-                  
-                const filter = (interaction) => interaction.isButton() && interaction.user.id != message.author.id && interaction.customId === 'Accept'  || interaction.customId === 'id' ;
-                const collector = message.channel.createMessageComponentCollector({ filter, time: 9000000 });
-                
-
-                collector.on('collect', async collected => {
- 
-
-                    if (collected.customId === 'Accept'){
-                    
-                    const  user = interaction.user.id;
-                    await collected.channel.send({ contents: `${user.id}`})
-                    }
-                    else if(collected.customId == 'id'){
-
-
-                    }
-
-                })
-               
-            }
-            else {
-
-                message.reply('You are in the wrong Channel bruh')
-            }
-            usedCommand.add(message.author.id);
-            setTimeout(() => {
-                usedCommand.delete(message.author.id);
-            }, 1800000); //You can set the ammount of the cooldown here! Its Formated to Miliseconds.
+            message.reply('You are in the wrong Channel bruh')
         }
+        usedCommand.add(message.author.id);
+        setTimeout(() => {
+            usedCommand.delete(message.author.id);
+        }, 1800000); //You can set the ammount of the cooldown here! Its Formated to Miliseconds.
     }
+}
+
+module.exports.config = {
+    name: "friendly",
+    aliases: [],
+    Description: 'Command',
+}
+
 }
 
 
