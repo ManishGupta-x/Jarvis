1a67042e9d92cb70583c015e48fc29a8440ead48
NightmareManish <manishgupta220402@gmail.com>
2021-10-15 19:03:06 +0530

    Update heavybase.js

--- src/commands/music/heavybase.js
+++ src/commands/music/heavybase.js
@@ -7,6 +7,7 @@ module.exports.run = async (client, message, args,Discord) => {
     if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
     let queue = await client.distube.getQueue(message);
     if(queue) { let filter = client.distube.setFilter(message,'heavybass');
+    client.distube.setVolume(message, 130);
     const embed = new Discord.MessageEmbed()
     .setColor('#1BE124')
     .setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/888052510416330762/wp7806858.png')
