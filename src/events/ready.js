f80b89ea64c6d4542e3ef99fa09d83f3eb8926ee
NightmareManish <manishgupta220402@gmail.com>
2021-10-16 10:57:16 +0530

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,13 +3,18 @@
 module.exports = (client) => {
     console.log('Jarvis is online!');
 
+var us = client.users.cache.size;
+us = us/1000;
+us = us.toString();
+us = us.slice(0,-2);
+us = parseInt(us);
 
 
     let statusarray = [
 
         `${client.guilds.cache.size}  Servers`,
         `${client.channels.cache.size} Channels`,
-        `${client.users.cache.size/1000} K Users`,
+        `${us} K Users`,
         `p!music for help `
 
     ]
