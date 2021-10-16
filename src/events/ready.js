dc9c29c34765d1593c02483d4de8ac4fa8aabedf
NightmareManish <manishgupta220402@gmail.com>
2021-10-16 10:48:05 +0530

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,30 +3,20 @@
 module.exports = (client) => {
     console.log('Jarvis is online!');
 
- let guildsize = client.guilds.cache.size ;
 
- if(guildsize > 1000){
 
-guildsize = guildsize/1000; 
-
- 
     let statusarray = [
 
-        `${guildsize} Servers`,
+        `${client.guilds.cache.size/1000} K Servers`,
         `${client.channels.cache.size} Channels`,
         `${client.users.cache.size} Users`,
         `p!music for help `
 
     ]
-
     setInterval(() => {
 
         client.user.setActivity(`${statusarray[Math.floor(Math.random() * statusarray.length)]}`, { type: 'WATCHING' })
 
     }, 10000)
 
-
-}
-
-    
 };
