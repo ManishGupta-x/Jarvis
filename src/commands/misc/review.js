eb44d39a579f1c14af8c13a8a7bf42b28abbefc6
NightmareManish <manishgupta220402@gmail.com>
2021-10-17 12:04:30 +0530

    Update review.js

--- src/commands/misc/review.js
+++ src/commands/misc/review.js
@@ -1,11 +1,17 @@
 
 const Discord = require("discord.js")
-
+const prefixModel = require("../../models/prefix")
+const mongoose = require("mongoose");
+const config = require('../../../settings.json');
+const { client } = require("discord.js");
+mongoose.connect(config.mongodb, { useNewUrlParser: true, useUnifiedTopology: true });
 module.exports.run = async (client, message, args,Discord) => {
     channelID = '876076486463987752';
-
+    const data = await prefixModel.findOne({
+        GuildID: message.guild.id
+    });
     if(!args[0]){
-        message.reply('Specify Your review pls \`p!review [type your review]\`')
+        message.reply(`Specify Your review pls \`${data.Prefix}review [type your review]\``)
         return;
     }    let issue = args.join(" ");
 client.channels.cache.get(`${channelID}`).send(`${client.users.cache.get(message.author.id).username}'s Review : ${issue}'`)
