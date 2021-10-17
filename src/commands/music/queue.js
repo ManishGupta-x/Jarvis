9d02b0922bd393423fc3caf61aa30abbb5c9b9fd
NightmareManish <manishgupta220402@gmail.com>
2021-10-17 09:58:42 +0530

    select menu

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -1,56 +1,66 @@
 const disTube = require('distube');
 const { MessageEmbed, Util } = require("discord.js")
+const { MessageActionRow, MessageSelectMenu } = require('discord.js')
 const Discord = require("discord.js")
 
-module.exports.run = async (client, message, args,Discord) => {
-    if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
-    var wallpapers = ["https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png", "https://cdn.discordapp.com/attachments/730714810614022228/882284561726308372/433536-Klayton-women-science_fiction-planet-Scandroid.png"
-        , "https://cdn.discordapp.com/attachments/730714810614022228/882284789145677854/Drum-Instrument-Neon-HD-Wallpapers-Free-Download.png", "https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png",
-        "https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png", "https://cdn.discordapp.com/attachments/730714810614022228/882284384202395678/neon-wallpaper-2008181520192-scaled.png", "https://cdn.discordapp.com/attachments/730714810614022228/882283761868357682/edm-house-music-dj-producer-beatmaker-wallpaper-hd-4k-desktop-6-2048x1080.png"]
-    var mu = wallpapers[Math.floor(Math.random() * wallpapers.length)];
-    let queue = await client.distube.getQueue(message);
-    if (!queue) {
-        message.reply('No queue Available');
-    } else if (queue) {
+const { row1, row2, row3, row4, row5 } = require('../../models/row')
 
-        const status = (queue) => `Volume: \`${queue.volume}%\` | Loop: \`${queue.repeatMode ? queue.repeatMode == 2 ? "Repeat queue" : "Repeat song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
+module.exports.run = async (client, message, args, Discord) => {
 
 
-        let y = Util.splitMessage(`__**Current Queue**__\n\n${queue.songs.map((song, id) => `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}`, { maxLength: 3500 })
+     if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
 
-        if (!y[1]) {
-            const embed9 = new Discord.MessageEmbed()
-                .setColor('RANDOM')
-                .setThumbnail(`${mu}`)
-                .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-                .setDescription(`${y[0]}\n\n${status(queue)}`)
-                .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
-                .setTimestamp();
-            await message.channel.send({ embeds: [embed9] })
-        }
+     var wallpapers = ["https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png", "https://cdn.discordapp.com/attachments/730714810614022228/882284561726308372/433536-Klayton-women-science_fiction-planet-Scandroid.png"
+          , "https://cdn.discordapp.com/attachments/730714810614022228/882284789145677854/Drum-Instrument-Neon-HD-Wallpapers-Free-Download.png", "https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png",
+          "https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png", "https://cdn.discordapp.com/attachments/730714810614022228/882284384202395678/neon-wallpaper-2008181520192-scaled.png", "https://cdn.discordapp.com/attachments/730714810614022228/882283761868357682/edm-house-music-dj-producer-beatmaker-wallpaper-hd-4k-desktop-6-2048x1080.png"]
 
-        if (y[1] && !y[2]) {
+     var mu = wallpapers[Math.floor(Math.random() * wallpapers.length)];
 
-            const embed9 = new Discord.MessageEmbed()
-                .setColor('RANDOM')
-                .setThumbnail(`${mu}`)
-                .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-                .setDescription(`${y[0]}`)
-                .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
-                .setTimestamp();
+     let queue = await client.distube.getQueue(message);
 
-            const embed10 = new Discord.MessageEmbed()
-                .setColor('RANDOM')
-                .setThumbnail(`${mu}`)
-                .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-                .setDescription(`${y[1]}\n\n${status(queue)}`)
-                .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
-                .setTimestamp();
-           message.channel.send({ embeds: [embed9] })
-           await message.channel.send({embeds : [embed10]})
-            if (y[2] && !y[3]) {
 
-                const embed9 = new Discord.MessageEmbed()
+
+
+
+     if (!queue) {
+          message.reply('No queue Available');
+     } else if (queue) {
+
+          const status = (queue) => `Volume: \`${queue.volume}%\` | Loop: \`${queue.repeatMode ? queue.repeatMode == 2 ? "Repeat queue" : "Repeat song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
+
+
+          let y = Util.splitMessage(`__**Current Queue**__\n\n${queue.songs.map((song, id) => `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}`, { maxLength: 3500 })
+
+          if (!y[1]) {
+               const embed9 = new Discord.MessageEmbed()
+                    .setColor('RANDOM')
+                    .setThumbnail(`${mu}`)
+                    .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+                    .setDescription(`${y[0]}\n\n${status(queue)}`)
+                    .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
+                    .setTimestamp();
+               if (queue.previousSongs.length < 1) {
+                    message.channel.send({ embeds: [embed9] })
+               } else if (queue.songs.length == 1 && queue.previousSongs.length >= 1) {
+                    message.channel.send({ embeds: [embed9], components: [row1] })
+
+
+               } else if(queue.songs.length ==2 && queue.previousSongs.length >= 1){
+                    message.channel.send({ embeds: [embed9], components: [row2] })
+               }
+               else if(queue.songs.length ==3 && queue.previousSongs.length >= 1){
+                    message.channel.send({ embeds: [embed9], components: [row3] })
+               }
+               else if(queue.songs.length == 4 && queue.previousSongs.length >= 1){
+                    message.channel.send({ embeds: [embed9], components: [row4] })
+               } else if(queue.songs.length == 5 && queue.previousSongs.length >= 1){
+                    message.channel.send({ embeds: [embed9], components: [row5] })
+               }
+     }
+
+          if (y[1] && !y[2]) {
+
+               const embed9 = new Discord.MessageEmbed()
                     .setColor('RANDOM')
                     .setThumbnail(`${mu}`)
                     .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
@@ -58,86 +68,105 @@ module.exports.run = async (client, message, args,Discord) => {
                     .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
                     .setTimestamp();
 
-                const embed10 = new Discord.MessageEmbed()
+               const embed10 = new Discord.MessageEmbed()
                     .setColor('RANDOM')
                     .setThumbnail(`${mu}`)
                     .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-                    .setDescription(`${y[1]}`)
+                    .setDescription(`${y[1]}\n\n${status(queue)}`)
                     .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
                     .setTimestamp();
+               message.channel.send({ embeds: [embed9] })
+               await message.channel.send({ embeds: [embed10], components: [row] })
+               if (y[2] && !y[3]) {
+
+                    const embed9 = new Discord.MessageEmbed()
+                         .setColor('RANDOM')
+                         .setThumbnail(`${mu}`)
+                         .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+                         .setDescription(`${y[0]}`)
+                         .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
+                         .setTimestamp();
+
+                    const embed10 = new Discord.MessageEmbed()
+                         .setColor('RANDOM')
+                         .setThumbnail(`${mu}`)
+                         .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+                         .setDescription(`${y[1]}`)
+                         .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
+                         .setTimestamp();
 
 
-                const embed12 = new Discord.MessageEmbed()
-                    .setColor('RANDOM')
-                    .setThumbnail(`${mu}`)
-                    .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-                    .setDescription(`${y[2]}\n\n${status(queue)}`)
-                    .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
-                    .setTimestamp();
-                 message.channel.send({ embeds: [embed9] });
-                await message.channel.send({embeds : [embed10]})
-                await message.channel.send({embeds : [embed12]})
-            }
-            if (y[3]) {
+                    const embed12 = new Discord.MessageEmbed()
+                         .setColor('RANDOM')
+                         .setThumbnail(`${mu}`)
+                         .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+                         .setDescription(`${y[2]}\n\n${status(queue)}`)
+                         .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
+                         .setTimestamp();
+                    message.channel.send({ embeds: [embed9] });
+                    await message.channel.send({ embeds: [embed10] })
+                    await message.channel.send({ embeds: [embed12], components: [row] })
+               }
+               if (y[3]) {
 
-                const embed9 = new Discord.MessageEmbed()
-                    .setColor('RANDOM')
-                    .setThumbnail(`${mu}`)
-                    .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-                    .setDescription(`${y[0]}`)
-                    .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
-                    .setTimestamp();
+                    const embed9 = new Discord.MessageEmbed()
+                         .setColor('RANDOM')
+                         .setThumbnail(`${mu}`)
+                         .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+                         .setDescription(`${y[0]}`)
+                         .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
+                         .setTimestamp();
 
-                const embed10 = new Discord.MessageEmbed()
-                    .setColor('RANDOM')
-                    .setThumbnail(`${mu}`)
-                    .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-                    .setDescription(`${y[1]}`)
-                    .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
-                    .setTimestamp();
-
-
-                const embed12 = new Discord.MessageEmbed()
-                    .setColor('RANDOM')
-                    .setThumbnail(`${mu}`)
-                    .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-                    .setDescription(`${y[2]}`)
-                    .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
-                    .setTimestamp();
+                    const embed10 = new Discord.MessageEmbed()
+                         .setColor('RANDOM')
+                         .setThumbnail(`${mu}`)
+                         .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+                         .setDescription(`${y[1]}`)
+                         .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
+                         .setTimestamp();
 
 
-                const embed14 = new Discord.MessageEmbed()
-                    .setColor('RANDOM')
-                    .setThumbnail(`${mu}`)
-                    .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-                    .setDescription(`${y[3]}\n\n${status(queue)}`)
-                    .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
-                    .setTimestamp();
-               
-                message.channel.send({ embeds: [embed9] });
-                await message.channel.send({embeds : [embed10]})
-                await message.channel.send({embeds : [embed12]})
-                await message.channel.send({embeds : [embed14]})
-
-            }
+                    const embed12 = new Discord.MessageEmbed()
+                         .setColor('RANDOM')
+                         .setThumbnail(`${mu}`)
+                         .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+                         .setDescription(`${y[2]}`)
+                         .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
+                         .setTimestamp();
 
 
+                    const embed14 = new Discord.MessageEmbed()
+                         .setColor('RANDOM')
+                         .setThumbnail(`${mu}`)
+                         .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+                         .setDescription(`${y[3]}\n\n${status(queue)}`)
+                         .setFooter(`Total Queue Time : ${queue.formattedDuration}`)
+                         .setTimestamp();
+
+                    message.channel.send({ embeds: [embed9] });
+                    await message.channel.send({ embeds: [embed10] })
+                    await message.channel.send({ embeds: [embed12] })
+                    await message.channel.send({ embeds: [embed14], components: [row] })
+
+               }
 
 
 
 
 
 
-        }
+
+
+          }
 
 
 
 
-    }
+     }
 }
 
 module.exports.config = {
-    name: "queue",
-    aliases: ['q'],
-    Description: 'Command',
+     name: "queue",
+     aliases: ['q'],
+     Description: 'Command',
 }
