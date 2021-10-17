299e090d02b25ca7923b3d3652bc771a22ca698c
NightmareManish <manishgupta220402@gmail.com>
2021-10-17 10:04:22 +0530

    Update row.js

--- src/models/row.js
+++ src/models/row.js
@@ -3,6 +3,7 @@ const disTube = require('distube');
 const { MessageEmbed, Util } = require("discord.js")
 const { MessageActionRow, MessageSelectMenu } = require('discord.js')
 const Discord = require("discord.js")
+const ClientManager = require(`../ClientManager`);
 const client = new ClientManager({
     partials: ["MESSAGE", "CHANNEL", "REACTION"],
     intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS', 'GUILD_PRESENCES', 'GUILD_VOICE_STATES', 'GUILD_MESSAGE_REACTIONS']
