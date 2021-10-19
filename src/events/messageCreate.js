e010252549ce29e19aa514bc630870788e149ddc
NightmareManish <manishgupta220402@gmail.com>
2021-10-19 13:32:25 +0530

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -14,23 +14,23 @@ module.exports = async (client, message) => {
 		GuildID: message.guild.id
 	});
 
-/*----------------------------------------------------------------------------------------------*/
-//args system	
-	
-    const messageArray = message.content.split(' ');
+	/*----------------------------------------------------------------------------------------------*/
+	//args system	
+
+	const messageArray = message.content.split(' ');
 	const cmd = messageArray[0];
 	const args = messageArray.slice(1);
 
 	if (message.author.bot || message.channel.type === 'dm') return;
 	if (message.author.bot) return;
 
-/*----------------------------------------------------------------------------------------------*/	
+	/*----------------------------------------------------------------------------------------------*/
 	if (message.mentions.members.first()) {
-	
+
 		if (message.mentions.members.first().id == '550267183503114250' || message.mentions.members.first().id == '852779571816562729') {
-	
+
 			if (d == 1 && message.author.id == '761159077639356446' && message.guild.id == '730714810064306188') {
-	
+
 				message.reply("Tumahra Darloo so rha h abhi 🥱, thodi der me ana 😴")
 			}
 			else if (d == 1) {
@@ -41,33 +41,33 @@ module.exports = async (client, message) => {
 	if (data) {
 
 		const prefix1 = data.Prefix;
-/*----------------------------------------------------------------------------------------------*/
+		/*----------------------------------------------------------------------------------------------*/
 		if (!message.content.startsWith(prefix1)) {
 
 			switch (message.content) {
-				case 'hey jarvis': if (message.author.id == '550267183503114250' || message.guild.id == '730714810064306188') { 
-					
-					message.reply("Yes Boss 🙃") 
-				
-				
+				case 'hey jarvis': if (message.author.id == '550267183503114250' || message.guild.id == '730714810064306188') {
+
+					message.reply("Yes Boss 🙃")
+
+
 				} else message.channel.send({ content: 'Hn bol bsdk 😒' })
 
 					break;
-				
-					case 'jarvis': if (message.author.id == '550267183503114250' || message.author.id == '761159077639356446' && message.guild.id == '730714810064306188') {
-						
-						message.reply("Yes Boss 🙃") 
-					
-					}else if (message.author.id == '761159077639356446') {
-					
-						message.channel.send({ content: 'Hn bolo Janeman 🙃' })
-			
-					}else message.channel.send({ content: 'Hn bol bsdk 😒' })
+
+				case 'jarvis': if (message.author.id == '550267183503114250' || message.author.id == '761159077639356446' && message.guild.id == '730714810064306188') {
+
+					message.reply("Yes Boss 🙃")
+
+				} else if (message.author.id == '761159077639356446') {
+
+					message.channel.send({ content: 'Hn bolo Janeman 🙃' })
+
+				} else message.channel.send({ content: 'Hn bol bsdk 😒' })
 
 					break;
 				case "adnan short story": message.channel.send("https://cdn.discordapp.com/attachments/794240262972243979/860556430490468362/VID_20210701_195116_318.mp4")
-					
-				break;
+
+					break;
 
 				case "jarvis google kr":
 					message.channel.send('Kya search krna h?')
@@ -80,28 +80,43 @@ module.exports = async (client, message) => {
 						return message.channel.send("error");
 
 					let res = result.body.items[0];
-					const embedd = new Discord.MessageEmbed()
-						.setColor('RANDOM')
-						.setTitle(res.title)
-						.setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/899921053877432370/ben-sweet-2LowviVHZ-E-unsplash-1.png ')
-						.setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-						.setDescription(res.snippet)
-						.setURL(res.link)
-						.setImage(res.pagemap.cse_image[0].src || res.pagemap.cse_thumbnail[0].src)
+					if (res.pagemap.cse_image[0].src || res.pagemap.cse_thumbnail[0].src) {
+						const embedd = new Discord.MessageEmbed()
+							.setColor('RANDOM')
+							.setTitle(res.title)
+							.setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/899921053877432370/ben-sweet-2LowviVHZ-E-unsplash-1.png ')
+							.setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+							.setDescription(res.snippet)
+							.setURL(res.link)
+							.setImage(res.pagemap.cse_image[0].src || res.pagemap.cse_thumbnail[0].src)
 
-						.setFooter(client.user.username, client.user.displayAvatarURL())
-						.setTimestamp();
-					message.channel.send({ embeds: [embedd] }).catch(error =>{
+							.setFooter(client.user.username, client.user.displayAvatarURL())
+							.setTimestamp();
+						message.channel.send({ embeds: [embedd] }).catch(error => {
 
-						message.channel.send('Error ,  try searching something else')
-					});
-					break;
+							message.channel.send('Error ,  try searching something else')
+						});
+					} else {
+						const embedd = new Discord.MessageEmbed()
+							.setColor('RANDOM')
+							.setTitle(res.title)
+							.setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/899921053877432370/ben-sweet-2LowviVHZ-E-unsplash-1.png ')
+							.setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+							.setDescription(res.snippet)
+							.setURL(res.link)
+							
+
+				        	.setFooter(client.user.username, client.user.displayAvatarURL())
+							.setTimestamp();
+						message.channel.send({ embeds: [embedd] })
+					}
+						break;
 
 
 
 				case "-friendly": await message.channel.send({ content: `<@&885896296739790919>` })
 					break;
-					
+
 				case "sleep mode on": if (message.author.id == '550267183503114250' || message.author.id == '852779571816562729') {
 					message.reply("Sweet dreams boss 🥱 ")
 					d = 1;
@@ -195,28 +210,46 @@ module.exports = async (client, message) => {
 				case "adnan short story": message.channel.send("https://cdn.discordapp.com/attachments/794240262972243979/860556430490468362/VID_20210701_195116_318.mp4")
 					break;
 				case "jarvis google kr":
-					message.reply('Kya search krna h?')
+					message.channel.send('Kya search krna h?')
 					const filter = m => m.author.id == message.author.id;
 					const collected = await message.channel.awaitMessages({ filter, max: 1, time: 60000, errors: ['time'] });
 					const query = collected.first().content;
 					let result = await superagent.get("https://customsearch.googleapis.com/customsearch/v1").query({ q: query, cx: '2a2bde9b24e897647', key: "AIzaSyDAXbVjTS4gOEJdNvXaOtLUnmi0zo0gVUA" })
 					if (!result.body.items) return message.channel.send(" Nothing Found! ")
 					if (result.status >= 400)
-						return message.channel.send("error").then(console.log(result.message));
+						return message.channel.send("error");
 
 					let res = result.body.items[0];
-					const embedd = new Discord.MessageEmbed()
-						.setColor('RANDOM')
-						.setTitle(res.title)
-						.setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/899921053877432370/ben-sweet-2LowviVHZ-E-unsplash-1.png ')
-						.setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-						.setDescription(res.snippet)
-						.setURL(res.link)
-						.setImage(res.pagemap.cse_image[0].src || res.pagemap.cse_thumbnail[0].src)
+					if (res.pagemap.cse_image[0].src || res.pagemap.cse_thumbnail[0].src) {
+						const embedd = new Discord.MessageEmbed()
+							.setColor('RANDOM')
+							.setTitle(res.title)
+							.setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/899921053877432370/ben-sweet-2LowviVHZ-E-unsplash-1.png ')
+							.setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+							.setDescription(res.snippet)
+							.setURL(res.link)
+							.setImage(res.pagemap.cse_image[0].src || res.pagemap.cse_thumbnail[0].src)
 
-						.setFooter(client.user.username, client.user.displayAvatarURL())
-						.setTimestamp();
-					message.channel.send({ embeds: [embedd] });
+							.setFooter(client.user.username, client.user.displayAvatarURL())
+							.setTimestamp();
+						message.channel.send({ embeds: [embedd] }).catch(error => {
+
+							message.channel.send('Error ,  try searching something else')
+						});
+					} else {
+						const embedd = new Discord.MessageEmbed()
+							.setColor('RANDOM')
+							.setTitle(res.title)
+							.setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/899921053877432370/ben-sweet-2LowviVHZ-E-unsplash-1.png ')
+							.setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+							.setDescription(res.snippet)
+							.setURL(res.link)
+							
+
+				        	.setFooter(client.user.username, client.user.displayAvatarURL())
+							.setTimestamp();
+						message.channel.send({ embeds: [embedd] })
+					}
 					break;
 				case "jarvis expose cvm": message.channel.send("https://cdn.discordapp.com/attachments/852811127889068033/871411602702860318/IMG_20210801_204737.jpg")
 					break;
@@ -224,7 +257,7 @@ module.exports = async (client, message) => {
 					break;
 				case "-friendly": await message.channel.send({ content: `<@&885896296739790919>` })
 					break;
-				
+
 				case "sleep mode on": if (message.author.id == '550267183503114250' || message.author.id == '852779571816562729') {
 					message.reply("Sweet dreams boss 🥱 ")
 					d = 1;
