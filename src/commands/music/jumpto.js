db840e49a99a8bd9d4b2c07dc3fcdf3e69b5ae28
NightmareManish <manishgupta220402@gmail.com>
2021-11-15 13:46:23 +0530

    Update jumpto.js

--- src/commands/music/jumpto.js
+++ src/commands/music/jumpto.js
@@ -10,14 +10,26 @@ module.exports.run = async (client, message, args,Discord) => {
     }
     if(queue){
 
-        if(!args[0]){
+        if(!args[0] || isNaN(args[0])){
 
             message.reply('Mention song number from queue to jump on');
 
         }else if(args[0]){
  
           
-            await client.distube.jump(message, Number(args[0]+1));
+            await client.distube.jump(message, Number(args[0]+1)).catch(error => {
+
+                const embed9 = new Discord.MessageEmbed()
+                    .setColor('RANDOM')
+                    .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
+                    .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+                    .setDescription(`Getting some issues try again `)
+        
+        
+                    .setFooter(`Report Manish<3 if you get this error`, client.user.displayAvatarURL())
+                    .setTimestamp();
+                message.channel.send({ embeds: [embed9] });
+            });
             const embed6 = new Discord.MessageEmbed()
             .setColor('RANDOM')
             .setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png')
