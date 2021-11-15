ac80ef08965edffd19a459d2a2043559898bb68f
NightmareManish <manishgupta220402@gmail.com>
2021-11-15 13:39:01 +0530

    Update jumpto.js

--- src/commands/music/jumpto.js
+++ src/commands/music/jumpto.js
@@ -15,8 +15,21 @@ module.exports.run = async (client, message, args,Discord) => {
             message.reply('Mention song number from queue to jump on');
 
         }else if(args[0]){
+ 
+            const num = args[0]+1;
+            await client.distube.jump(message, num);
+            const embed6 = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png')
+            .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+            .setDescription(`Jumped to : ${Number(args[0])}`)
 
-            await client.distube.jump(message, args[0]+1);
+
+            .setFooter(client.user.username, client.user.displayAvatarURL())
+            .setTimestamp();
+        message.channel.send({ embeds: [embed6] });
+
+            
         }
         
     }
