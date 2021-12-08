03f4aa8bbd9888f12dd85b03749a4da953bc3492
NightmareManish <manishgupta220402@gmail.com>
2021-12-08 19:27:56 +0530

    Update error.js

--- src/events/error.js
+++ src/events/error.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const ClientManager = require(`./src/ClientManager`);
 const client = new ClientManager({
     partials: ["MESSAGE", "CHANNEL", "REACTION"],
     intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS', 'GUILD_PRESENCES', 'GUILD_VOICE_STATES', 'GUILD_MESSAGE_REACTIONS']
