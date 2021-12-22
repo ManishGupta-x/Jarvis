31a2318ffca2ca7f5566dde3e3b723b96bbfed8a
NightmareManish <manishgupta220402@gmail.com>
2021-12-22 13:14:56 +0530

    Update mytasks.js

--- src/commands/todo/mytasks.js
+++ src/commands/todo/mytasks.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-
+const { MessageActionRow, MessageButton } = require('discord.js');
 const mongoose = require("mongoose");
 const config = require("../../../settings.json");
 mongoose.connect(config.mongodb, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -50,7 +50,7 @@ module.exports.run = async (client, message, args, Discord) => {
 				"Jarvis",
 				"https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp"
 			)
-			.setDescription(`Do you want `)
+			.setDescription(`Do you want Send your tasks in Dm or in this channel?`)
 
 			.setFooter(client.user.username, client.user.displayAvatarURL())
 			.setTimestamp();
@@ -60,11 +60,29 @@ module.exports.run = async (client, message, args, Discord) => {
 		const filter1 = (interaction) =>
 			(interaction.isButton() &&
 				interaction.user.id == message.author.id &&
-				interaction.customId === "yes") ||
-			interaction.customId === "no";
+				interaction.customId === "Dm") ||
+			interaction.customId === "Channel";
 
-		const collector = message.channel.createMessageComponentCollector({ filter1, time: 60000 });
-		message.channel.send({ embeds: [embed1] });
+		const collector = message.channel.createMessageComponentCollector({ filter1, time: 10000 });
+		
+
+     collector.on('collect',async collected =>{
+
+          if(collected.customId =='Dm'){
+
+            message.author.send({embeds : [embed1]});
+
+            message.channel.send({content : 'Your Tasks have been uploaded to your dm !'})
+          }else if(collected.customId == 'Channel'){
+
+             message.channel.send({embeds : [embed1] });
+          }
+
+     })
+
+
+
+
 	} else if (!data) {
 		const embed2 = new Discord.MessageEmbed()
 			.setColor("RANDOM")
