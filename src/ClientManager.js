fd3bda965f0c48026e716f54bb65f79189a0419f
NightmareManish <manishgupta220402@gmail.com>
2021-12-26 21:05:47 +0530

    Update ClientManager.js

--- src/ClientManager.js
+++ src/ClientManager.js
@@ -106,7 +106,7 @@ module.exports = class ClientManager extends Client {
 			.on("searchDone", (message) => message.channel.send(`Search Done!`))
 			.on("searchCancel", (message) => message.channel.send(`Searching canceled`))
 			.on("searchInvalidAnswer", (message) => message.channel.send(`You answered an invalid `))
-			.on("error" , (message) => message.channel.send({contents : 'something went wrong with song try searching another'}))
+			.on("error" , (message) => message.channel.send({content : 'something went wrong with song try searching another'}))
 			.on("searchNoResult", (message, query) =>
 				message.channel.send(`No result found for ${query}!`)
 			)
