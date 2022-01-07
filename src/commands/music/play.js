68d957edf4ddc9b6df1d05f7eb5e0958bd0a1755
NightmareManish <manishgupta220402@gmail.com>
2022-01-07 17:43:24 +0530

    Update play.js

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -10,19 +10,19 @@ module.exports.run = async (client, message, args,Discord) => {
     const music = args.join(" ");
 
     client.distube.play(message, music)
-    .catch(error => {
+    // .catch(error => {
 
-        const embed9 = new Discord.MessageEmbed()
-            .setColor('RANDOM')
-            .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
-            .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-            .setDescription(`Getting some issues try again `)
+    //     const embed9 = new Discord.MessageEmbed()
+    //         .setColor('RANDOM')
+    //         .setThumbnail('https://cdn.discordapp.com/attachments/610950416498425886/848609872521461800/thumb-1920-554935.png')
+    //         .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+    //         .setDescription(`Getting some issues try again `)
 
 
-            .setFooter(`Report Manish<3 if you get this error`, client.user.displayAvatarURL())
-            .setTimestamp();
-        message.channel.send({ embeds: [embed9] });
-    });
+    //         .setFooter(`Report Manish<3 if you get this error`, client.user.displayAvatarURL())
+    //         .setTimestamp();
+    //     message.channel.send({ embeds: [embed9] });
+    // });
 
 }
 
