9209d0fbc6d7c854ea3b54fb0b36bc3dfa90ef27
NightmareManish <manishgupta220402@gmail.com>
2022-04-24 11:21:41 +0530

    make it better

--- src/commands/music/disconnect.js
+++ src/commands/music/disconnect.js
@@ -15,7 +15,9 @@ module.exports.run = async (client, message, args,Discord) => {
     .setColor('RANDOM')
     .setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png')
     .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
-    .setDescription(`Succesfully Disconnected ! I hope you enjoyed 💘 ! Do write your reviews by ${data.Prefix}review`)
+    .setDescription(`Succesfully Disconnected ! I hope you enjoyed 💘 ! Do write your reviews by ${data.Prefix}review
+    
+    Do Donate Us any amount you feel like , will be appreciated 💖. 100 % will be invested in Maintance of the Server of the Bot. Type p!donate to donate 🥰  `)
     
     
     .setFooter(client.user.username, client.user.displayAvatarURL())
@@ -25,7 +27,10 @@ module.exports.run = async (client, message, args,Discord) => {
         const embed = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setThumbnail('https://cdn.discordapp.com/attachments/730714810614022228/882284227457073172/thumb2-music-neon-icon-4k-violet-background-neon-symbols-music.png')
-        .setAuthor('Jarvis', 'https://cdn.discordapp.com/avatars/778267007439077396/66fa9525d6e9af153dac819fc04d3ee1.webp')
+        .setAuthor(
+            "Jarvis",
+            "https://cdn.discordapp.com/avatars/778267007439077396/b3f9ab1c6342de220b333fcbdff93ec5.png?size=256"
+        )
         .setDescription(`Succesfully Disconnected ! I hope you enjoyed 💘 ! Do write your reviews by p!review`)
         
         
